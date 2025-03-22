96b9613434d2616fc556a1851bf6c837d34d380c
bae080311 <s24024@gsm.hs.kr>
2025-03-22 20:49:45 +0900

    Chore: colors, font

--- packages/tailwind-config/tailwind.config.ts
+++ packages/tailwind-config/tailwind.config.ts
@@ -4,9 +4,42 @@ import type { Config } from "tailwindcss";
 const config: Omit<Config, "content"> = {
   theme: {
     extend: {
-      backgroundImage: {
-        "glow-conic":
-          "conic-gradient(from 180deg at 50% 50%, #2a8af6 0deg, #a853ba 180deg, #e92a67 360deg)",
+      fontWeights: {
+        Semibold: 600, // title, body1, 2
+        regular: 400, // body3, label
+      },
+      fontSize: {
+        titleLarge: "3rem",
+        titleMedium: "2.25rem",
+        titleSmall: "1.5rem",
+        body1: "1.125rem",
+        body: "1rem",
+        label: "0.875rem",
+      },
+      lineHeight: {
+        high: "3.6rem",
+        medium: "1.8rem",
+        small: "1.05rem",
+      },
+      colors: {
+        success: "#3870F9",
+        error: "#DF454A",
+        primary: "#5E97FC",
+        secondary: "#DFEAFE",
+        third: "#385B97",
+        forth: "#263C65",
+        scoreBackground: "#F3F3F3",
+        gray: {
+          100: "#E6E6E7",
+          200: "#CDCDCF",
+          300: "#DBDCDE",
+          400: "#9B9C9F",
+          500: "#828387",
+          600: "#68696C",
+          700: "#4E4F51",
+          800: "#343436",
+          900: "#1A1A1B",
+        },
       },
     },
   },
