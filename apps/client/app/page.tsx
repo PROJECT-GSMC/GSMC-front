6865e87366aebf3d8644f9ff7eac466f1d44bab1
Dino0204 <s24020@gsm.hs.kr>
2025-03-25 20:40:38 +0900

    Feat: Login Page

--- apps/client/app/page.tsx
+++ apps/client/app/page.tsx
@@ -1,3 +1,10 @@
-export default function Page() {
-  return <div></div>;
+import { LoginForm } from "../src/widgets/auth/loginForm"
+
+export default function page() {
+  return (
+    <div>
+      <LoginForm />
+    </div>
+  )
 }
+
