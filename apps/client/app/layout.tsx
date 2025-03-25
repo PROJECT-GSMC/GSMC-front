be524cdc9df34e611d32dff53b9bc2cf154841aa
Dino0204 <s24020@gsm.hs.kr>
2025-03-25 20:40:05 +0900

    💄 Feat: 기본 레이아웃 설정

--- apps/client/app/layout.tsx
+++ apps/client/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="flex flex-col h-screen justify-center items-center bg-[#DFEAFE]">{children}</body>
     </html>
   );
 }
