dd9ad9eb4a0745e10d5517cfad59cb4e4e6fe6a5
Dino0204 <s24020@gsm.hs.kr>
2025-03-27 16:55:22 +0900

    🎨 Improve: 메인 레이아웃

--- apps/client/app/layout.tsx
+++ apps/client/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="flex flex-col h-screen justify-center items-center bg-[#DFEAFE]">{children}</body>
+      <body className="flex flex-col h-screen justify-center items-center bg-main-100">{children}</body>
     </html>
   );
 }
