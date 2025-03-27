81d6bca83090362e34a7815e0ac9db71a852b450
Dino0204 <s24020@gsm.hs.kr>
2025-03-27 16:54:39 +0900

    Feat: 공용 버튼 컴포넌트

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,35 +1,26 @@
 "use client"
-import { useEffect, useState } from "react";
+import { useMemo } from "react";
 
 interface ButtonProps {
   label: string;
   isActive?: boolean;
-  type?: "primary" | "secondary";
+  type: "primary" | "secondary";
+  onClick?: () => void;
 }
 
-export const Button = ({ label, isActive, type }: ButtonProps) => {
+export const Button = ({ label, isActive = false, type, onClick }: ButtonProps) => {
+  const buttonStyle = useMemo(() => {
+    const primary = isActive ? "bg-main-500 text-white" : "border-gray-200 text-gray-500";
+    const secondary = isActive ? "border-tropicalblue-400 text-tropicalblue-400" : "border-gray-300 text-gray-800";
 
-  const [style, setStyle] = useState("")
-
-  useEffect(() => {
-    if (type === "primary") {
-      if (isActive) {
-        setStyle("bg-main-500 text-white")
-      } else {
-        setStyle("bg-gray-200 text-gray-500")
-      }
-    } else if (type === "secondary") {
-      if (isActive) {
-        setStyle("border-tropicalblue-400 text-tropicalblue-400")
-      } else {
-        setStyle("border-gray-300 text-gray-800")
-      }
-    }
-  }, [])
+    return `${type == "primary" ? primary : secondary}`;
+  }, [isActive, type]);
 
   return (
-    <button className={`flex py-[0.8125rem] px-[1.375rem] border justify-center items-center self-stretch rounded-[0.75rem] ${style}`}>
+    <button className={`flex py-[0.8125rem] px-[1.375rem] border justify-center items-center self-stretch rounded-[0.75rem] ${buttonStyle}`} disabled={!isActive}>
       {label}
     </button>
   )
 }
+
+
