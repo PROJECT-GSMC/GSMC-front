0c88d47f5176762b749148de8fd2333f49fdd3cd
Dino0204 <s24020@gsm.hs.kr>
2025-03-27 20:38:01 +0900

    📝Update: 불필요한 경로 삭제

--- apps/client/tailwind.config.ts
+++ apps/client/tailwind.config.ts
@@ -5,7 +5,6 @@ const config: Pick<Config, "content" | "presets" | "theme"> = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "../../packages/ui/src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
