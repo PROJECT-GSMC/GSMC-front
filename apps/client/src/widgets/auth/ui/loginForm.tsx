19de21d3114d7ffe698ce02403f8970fe5e06e3e
Dino0204 <s24020@gsm.hs.kr>
2025-03-27 21:01:28 +0900

    🐛 Fix; LoginForm

--- apps/client/src/widgets/auth/ui/loginForm.tsx
+++ apps/client/src/widgets/auth/ui/loginForm.tsx
@@ -2,41 +2,64 @@
 import { Input } from "@repo/ui/input"
 import { Button } from "@repo/ui/button"
 import { useForm, Controller } from "react-hook-form"
-import { useEffect, useState } from "react"
+
+interface LoginFormProps {
+  email: string;
+  password: string;
+}
 
 export const LoginForm = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: {
+      isSubmitting,
+      isSubmitted,
+      isDirty,
+      isValid,
+      errors
+    }
+  } = useForm<LoginFormProps>({ defaultValues: { email: '', password: '' } })
 
-  const { control, handleSubmit, formState: { isSubmitting, isSubmitted, errors } } = useForm({ defaultValues: { email: '', password: '' } })
-
-  const [isFormValid, setIsFormValid] = useState(false)
-
-  useEffect(() => {
-    setIsFormValid(Object.keys(errors).length === 0)
-  }, [errors])
+  const onSubmit = (data: LoginFormProps) => {
+    alert(JSON.stringify(data))
+  }
 
   return (
-    <form onSubmit={handleSubmit((data) => { alert(JSON.stringify(data)) })} className="flex pt-[5.625rem] p-[6.25rem] justify-center items-center rounded-[1.25rem] bg-white ">
+    <form onSubmit={handleSubmit(onSubmit)} className="flex pt-[5.625rem] p-[6.25rem] justify-center items-center rounded-[1.25rem] bg-white ">
       <div className="flex flex-col items-center gap-[3.625rem]">
         <h1 className="text-[2.25rem] font-bold">LOG IN</h1>
         <div className="flex flex-col gap-[0.75rem] items-start self-stretch">
           <Controller
             name="email"
             control={control}
-            aria-invalid={errors.email ? "true" : "false"}
-            rules={{ required: "이메일을 필수로 입력해야 합니다.", pattern: { value: /^s\d{5}@gsm\.hs\.kr$/, message: "이메일을 다시 한 번 확인해주세요" } }}
+            rules={
+              {
+                required: "이메일을 필수로 입력해야 합니다.",
+                pattern: {
+                  value: /^s\d{5}@gsm\.hs\.kr$/,
+                  message: "이메일을 다시 한 번 확인해주세요"
+                }
+              }}
             render={({ field }) => <Input label="이메일" type="email" {...field} />}
           />
-          {errors.email && <small role="alert" className="text-error-500">{errors.email.message}</small>}
+          {<small role="alert" className="text-error-500">{errors.email?.message}</small>}
           <Controller
             name="password"
             control={control}
-            rules={{ required: "비밀번호을 필수로 입력해야 합니다.", pattern: { value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,12}$/, message: "비밀번호를 다시 한 번 확인해주세요" } }}
+            rules={
+              {
+                required: "비밀번호을 필수로 입력해야 합니다.",
+                pattern: {
+                  value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,12}$/,
+                  message: "비밀번호를 다시 한 번 확인해주세요"
+                }
+              }}
             render={({ field }) => <Input label="비밀번호" type="password" {...field} />}
           />
-          {errors.password && <small role="alert" className="text-error-500">{errors.password.message}</small>}
+          {<small role="alert" className="text-error-500">{errors.password?.message}</small>}
         </div>
-        <button onClick={() => setIsFormValid(!isFormValid)}>아니</button>
-        <Button label="로그인" type="primary" isActive={isFormValid} />
+        <Button label="로그인" type="primary" isActive={isDirty && isValid} />
       </div>
     </form>
   )
