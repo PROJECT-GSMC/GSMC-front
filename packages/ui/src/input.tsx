5aa4628f155f9f73de743b1f2659389d14349413
Dino0204 <s24020@gsm.hs.kr>
2025-03-31 23:50:20 +0900

    🎨 Improve: signup 페이지를 위한 공통 widget 분리

--- packages/ui/src/input.tsx
+++ packages/ui/src/input.tsx
@@ -4,13 +4,14 @@ interface InputProps {
   label: string;
   value: string;
   type?: "text" | "password" | "email";
+  error?: string
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export const Input = (
-  ({ label, type = "text", value, onChange }: InputProps) => {
+  ({ label, type = "text", value, error, onChange }: InputProps) => {
     return (
-      <div className="flex flex-col w-full gap-[0.25rem] items-start">
+      <div className="flex flex-col w-full">
         <label htmlFor={label} className="text-label">
           {label}
         </label>
@@ -22,6 +23,7 @@ export const Input = (
           onChange={onChange}
           className="p-[1rem] rounded-[0.5rem] border bg-white ui-outline-gray-600"
         />
+        <small role="alert" className="text-red-500">{error}</small>
       </div>
     );
   }
