4648424bdeaeef688d67fde98a0f7bddbf8bc252
Dino0204 <s24020@gsm.hs.kr>
2025-03-31 23:12:21 +0900

    🚚Move: Login

--- apps/client/app/login/page.tsx
+++ apps/client/app/login/page.tsx
@@ -1,7 +1,9 @@
+import { LoginForm } from "../../src/widgets/auth/ui/loginForm"
+
 export default function page() {
   return (
     <div className="flex justify-center items-center h-screen bg-tropicalblue-100">
-
+      <LoginForm />
     </div>
   )
 }
