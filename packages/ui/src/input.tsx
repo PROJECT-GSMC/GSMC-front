42a5b183341ce4fd76f4de0e7be1555298f93621
Dino0204 <s24020@gsm.hs.kr>
2025-03-31 23:15:40 +0900

    💄 Update: Input 수정

--- packages/ui/src/input.tsx
+++ packages/ui/src/input.tsx
@@ -2,29 +2,25 @@ import React from "react";
 
 interface InputProps {
   label: string;
-  type?: "text" | "password" | "email";
   value: string;
+  type?: "text" | "password" | "email";
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  onBlur: () => void;
-  ref: React.Ref<HTMLInputElement>;
 }
 
-export const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ label, type = "text", value, onChange, onBlur }, ref) => {
+export const Input = (
+  ({ label, type = "text", value, onChange }: InputProps) => {
     return (
       <div className="flex flex-col w-full gap-[0.25rem] items-start">
         <label htmlFor={label} className="text-label">
           {label}
         </label>
         <input
-          type={type}
-          name={label}
           id={label}
+          name={label}
+          type={type}
           value={value}
           onChange={onChange}
-          onBlur={onBlur}
-          ref={ref}
-          className="flex w-[25rem] p-[1rem] items-center gap-[0.625rem] rounded-[0.5rem] border bg-white outline-main-500"
+          className="p-[1rem] rounded-[0.5rem] border bg-white ui-outline-gray-600"
         />
       </div>
     );
