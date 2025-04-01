3c67db010fc1f10140bfdee7e8682de65ae854d9
bae080311 <s24024@gsm.hs.kr>
2025-04-02 00:15:26 +0900

    Remove: content

--- apps/admin/tailwind.config.ts
+++ apps/admin/tailwind.config.ts
@@ -3,8 +3,7 @@
 import type { Config } from "tailwindcss";
 import sharedConfig from "@repo/tailwind-config";
 
-const config: Pick<Config, "content" | "presets"> = {
-  content: ["./app/**/*.tsx"],
+const config: Pick<Config, "presets"> = {
   presets: [sharedConfig],
 };
 
