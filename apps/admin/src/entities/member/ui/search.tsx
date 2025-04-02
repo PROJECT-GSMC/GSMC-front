b52f4ae6c022d075a813b69cc37ed83dead65a35
bae080311 <s24024@gsm.hs.kr>
2025-04-02 18:18:59 +0900

    Feat: placeholder

--- apps/admin/src/entities/member/ui/search.tsx
+++ apps/admin/src/entities/member/ui/search.tsx
@@ -4,6 +4,7 @@ export const Search = () => {
   return (
     <div className=" relative">
       <input
+        placeholder="학생을 검색하세요"
         className="bg-tropicalblue-50 p-[0.75rem] pl-10 rounded-[0.75rem] w-full"
         type="text"
       />
