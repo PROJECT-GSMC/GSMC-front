c747ff0220ec838439562676d52732a00360e4d8
bae080311 <s24024@gsm.hs.kr>
2025-04-02 18:17:12 +0900

    Style: cursor pointer

--- apps/admin/src/widget/member/ui/memberList.tsx
+++ apps/admin/src/widget/member/ui/memberList.tsx
@@ -2,14 +2,28 @@ import MemberCard from "../../../entities/member/ui/memberCard";
 
 const MemberList = () => {
   return (
-    <div className="w-[30%] bg-main-100 py-[2.25rem] px-[1.38rem] rounded-[1.25rem]">
+    <div className="w-[573px] h-[739px] bg-main-100 py-[2.25rem] px-[1.38rem] rounded-[1.25rem]">
       <div className="flex items-center mb-[2.2rem]">
         <h4 className="text-main-700 text-titleSmall">17</h4>
         <span className="text-body2">명</span>
       </div>
-      <MemberCard />
-      <MemberCard />
-      <MemberCard />
+      <div className="overflow-y-auto flex flex-col h-[600px]">
+        <MemberCard />
+        <MemberCard />
+        <MemberCard />
+        <MemberCard />
+        <MemberCard />
+        <MemberCard />
+        <MemberCard />
+        <MemberCard />
+        <MemberCard />
+        <MemberCard />
+        <MemberCard />
+        <MemberCard />
+        <MemberCard />
+        <MemberCard />
+        <MemberCard />
+      </div>
     </div>
   );
 };
