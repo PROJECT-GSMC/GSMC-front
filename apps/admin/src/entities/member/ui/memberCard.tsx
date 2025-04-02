1afcdad11563f8a85502e9d8ddd9f089f85e2050
bae080311 <s24024@gsm.hs.kr>
2025-04-02 18:19:16 +0900

    Style: pointer

--- apps/admin/src/entities/member/ui/memberCard.tsx
+++ apps/admin/src/entities/member/ui/memberCard.tsx
@@ -1,6 +1,6 @@
 const MemberCard = () => {
   return (
-    <div className="flex justify-between w-[100%] text-gray-600 text-label py-[1.5rem] px-[2rem]">
+    <div className="flex justify-between w-[100%] cursor-pointer text-gray-600 text-label py-[1.5rem] px-[2rem]">
       <span>모태환</span>
       <span>2104</span>
     </div>
