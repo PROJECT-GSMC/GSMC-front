a2e924378738266dea64524ee86285d857d9b11d
Dino0204 <s24020@gsm.hs.kr>
2025-04-03 17:35:13 +0900

    🎨 Improve: Button 과 Const 코드 분리

---
+++ apps/client/src/shared/consts/button/index.ts
@@ -0,0 +1,15 @@
+export const variantStyles = {
+  blue: {
+    default: "bg-tropicalblue-500 text-white",
+    active: "bg-tropicalblue-900 text-white",
+    disabled: "bg-gray-200 text-gray-500 cursor-not-allowed",
+  },
+  skyblue: {
+    default:
+      "bg-white border border-tropicalblue-400 text-tropicalblue-400 hover:bg-tropicalblue-500 hover:text-white hover:border-tropicalblue-500",
+    active:
+      "bg-gray-300 border border-tropicalblue-400 text-tropicalblue-400 hover:bg-tropicalblue-500 hover:text-white hover:border-tropicalblue-500",
+    disabled:
+      "bg-white border border-gray-500 text-gray-500 cursor-not-allowed",
+  },
+};
