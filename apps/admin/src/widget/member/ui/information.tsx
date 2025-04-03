c4edf6a18e7c84be4328989cbb5389d879a75b88
bae080311 <s24024@gsm.hs.kr>
2025-04-03 13:09:56 +0900

    Fix: layout

--- apps/admin/src/widget/member/ui/information.tsx
+++ apps/admin/src/widget/member/ui/information.tsx
@@ -2,14 +2,16 @@ import { Button } from "@repo/ui/button";
 
 export const Information = () => {
   return (
-    <div className="w-[350px] bg-main-100 py-[2.25rem] rounded-[1.25rem] h-[90vh] px-[2.45rem]">
+    <div className="w-[350px] bg-main-100 py-[2.25rem] flex flex-col justify-between rounded-[1.25rem] h-[90vh] px-[2.45rem]">
       <h3 className="text-titleSmall text-main-700 mb-[3.5rem]">인적사항</h3>
-      <div className="flex flex-col gap-[0.5rem]">
-        <span className="text-body1 text-gray-600">모태환</span>
-        <small className="text-body2 text-gray-600">2학년 1반 04번</small>
-      </div>
-      <div className="text-main-700 mt-[1.25rem] mb-[2.56rem] text-titleMedium w-[17rem] h-[20rem] flex items-center justify-center rounded-[0.75rem] bg-white">
-        2300점
+      <div>
+        <div className="flex flex-col gap-[0.5rem]">
+          <span className="text-body1 text-gray-600">모태환</span>
+          <small className="text-body2 text-gray-600">2학년 1반 04번</small>
+        </div>
+        <div className="text-main-700 mt-[1.25rem] mb-[2.56rem] text-titleMedium w-[17rem] h-[20rem] flex items-center justify-center rounded-[0.75rem] bg-white">
+          2300점
+        </div>
       </div>
       <div className="flex flex-col gap-[0.75rem]">
         <Button type="primary" isActive label="글 보러가기" />
