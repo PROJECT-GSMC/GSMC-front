e9a287729b872da3155771e944e250c4a8c8bf74
bae080311 <s24024@gsm.hs.kr>
2025-04-03 13:02:01 +0900

    Feat: memberList ui

--- apps/admin/src/widget/member/ui/memberList.tsx
+++ apps/admin/src/widget/member/ui/memberList.tsx
@@ -2,12 +2,12 @@ import MemberCard from "../../../entities/member/ui/memberCard";
 
 const MemberList = () => {
   return (
-    <div className="w-[573px] h-[739px] bg-main-100 py-[2.25rem] px-[1.38rem] rounded-[1.25rem]">
+    <div className="w-[573px] h-[90vh] bg-main-100 py-[2.25rem] px-[1.38rem] rounded-[1.25rem]">
       <div className="flex items-center mb-[2.2rem]">
         <h4 className="text-main-700 text-titleSmall">17</h4>
         <span className="text-body2">명</span>
       </div>
-      <div className="overflow-y-auto flex flex-col h-[600px]">
+      <div className="overflow-y-auto flex flex-col h-full">
         <MemberCard />
         <MemberCard />
         <MemberCard />
