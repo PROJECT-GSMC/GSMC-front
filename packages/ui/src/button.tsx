fa3d34576fa1bbee7bc7560e38130c07adc78248
Dino0204 <s24020@gsm.hs.kr>
2025-04-03 18:10:17 +0900

    🐛 Fix: 경로 문제

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -3,6 +3,7 @@ import { useMemo } from "react";
 
 type Enable = "default"
 type Disable = "disabled" | "active"
+
 interface CustomButton {
   default: string,
   active: string,
@@ -20,9 +21,7 @@ interface ButtonProps {
 export const Button = ({ variant, isActive, label, width, onClick }: ButtonProps) => {
   const state: Enable | Disable = isActive ? "default" : onClick ? "active" : "disabled"
   const baseStyle = "flex py-[0.8125rem] w-full text-body2 px-[1.375rem] justify-center items-center self-stretch rounded-[0.75rem] transition-colors";
-  const buttonStyle = useMemo(() => {
-    return variant[state]
-  }, [variant, state]);
+  const buttonStyle = useMemo(() => variant[state], [variant, state]);
 
   return (
     <button
