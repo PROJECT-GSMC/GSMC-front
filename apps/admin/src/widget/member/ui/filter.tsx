f791f34cde9494b67639d0966888a780fbdb8a90
Dino0204 <s24020@gsm.hs.kr>
2025-04-04 09:46:42 +0900

    🚑 Hotfix: main -> tropicalblue 스타일

--- apps/admin/src/widget/member/ui/filter.tsx
+++ apps/admin/src/widget/member/ui/filter.tsx
@@ -4,11 +4,11 @@ import { variantStyles } from "../../../../../../packages/ui/src/consts/button";
 
 export const Filter = () => {
   return (
-    <div className="w-[350px] bg-main-100 py-[2.25rem] rounded-[1.25rem] h-[90vh] px-[2.45rem]">
-      <h3 className="text-titleSmall text-main-700 mb-[2.2rem]">필터</h3>
+    <div className="w-[350px] bg-tropicalblue-100 py-[2.25rem] rounded-[1.25rem] h-[90vh] px-[2.45rem]">
+      <h3 className="text-titleSmall text-tropicalblue-700 mb-[2.2rem]">필터</h3>
       <Search />
       <div>
-        <h5 className="text-main-700 text-label mt-[3.06rem]">학년</h5>
+        <h5 className="text-tropicalblue-700 text-label mt-[3.06rem]">학년</h5>
         <div className="flex  justify-between mt-[1.5rem]">
           <Button variant={variantStyles["skyblue"]} width="w-[25%]" isActive label="1" />
           <Button variant={variantStyles["skyblue"]} width="w-[25%]" isActive label="2" />
@@ -16,7 +16,7 @@ export const Filter = () => {
         </div>
       </div>
       <div className="flex flex-col gap-[1.38rem] mt-[2.19rem]">
-        <h5 className="text-main-700 text-label">반</h5>
+        <h5 className="text-tropicalblue-700 text-label">반</h5>
         <div className="flex justify-between mt">
           <Button variant={variantStyles["skyblue"]} width="w-[25%]" isActive label="1" />
           <Button variant={variantStyles["skyblue"]} width="w-[25%]" isActive label="2" />
