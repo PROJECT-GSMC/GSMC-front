1a082a1a7cc391adf97c1993edcb0c1ec1e5b612
bae080311 <s24024@gsm.hs.kr>
2025-04-06 14:59:36 +0900

    Fix: header width

--- apps/admin/src/shared/ui/header.tsx
+++ apps/admin/src/shared/ui/header.tsx
@@ -2,7 +2,7 @@ import TextLogo from "@repo/ui/textLogo";
 
 const Header = () => {
   return (
-    <header className="py-[1.4rem] flex justify-center items-center border-b">
+    <header className="py-[1.4rem] flex w-full justify-center items-center border-b">
       <TextLogo />
     </header>
   );
