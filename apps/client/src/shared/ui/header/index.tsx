867aa8fed80096c32f721ec13914a844b811dbe8
bae080311 <s24024@gsm.hs.kr>
2025-04-07 09:57:19 +0900

    Feat: header 경로

--- apps/client/src/shared/ui/header/index.tsx
+++ apps/client/src/shared/ui/header/index.tsx
@@ -1,13 +1,12 @@
-import Image from "next/image";
-import Textlogo from "../../asset/svg/textLogo.svg";
+import TextLogo from "@repo/ui/textLogo";
 import Link from "next/link";
 
 const Header = () => {
   return (
     <header className="w-full py-[1.38rem] flex justify-around">
-      {/* <Link href={"/"}>
-        <Image src={Textlogo} id="image" alt="logo" />
-      </Link> */}
+      <Link href="/">
+        <TextLogo />
+      </Link>
       <ul className="gap-[2rem] flex text-label text-gray-500 items-center text-body3">
         <Link href="/calculate">점수 계산</Link>
         <Link href="/descrition">설명</Link>
