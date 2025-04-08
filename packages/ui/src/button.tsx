3f6866114a244f66f4fd7d0c70695c7be303572b
Dino0204 <s24020@gsm.hs.kr>
2025-04-08 20:44:10 +0900

    🎨 Improve: 버튼 컴포넌트 타입 & 로직

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,32 +1,26 @@
 "use client";
 import { useMemo } from "react";
+import { variantStyles } from "./consts/button";
 
-type Enable = "default"
-type Disable = "disabled" | "active"
-
-interface CustomButton {
-  default: string,
-  active: string,
-  disabled: string
-}
+type ButtonState = "default" | "active" | "disabled";
 
 interface ButtonProps {
   label: string;
-  variant: CustomButton;
-  width?: string
+  variant: keyof typeof variantStyles;
+  width?: string;
   isActive?: boolean;
+  hover?: boolean;
   onClick?: () => void;
 }
 
-export const Button = ({ variant, isActive, label, width, onClick }: ButtonProps) => {
-  const state: Enable | Disable = isActive ? "default" : onClick ? "active" : "disabled"
+export const Button = ({ variant, isActive, label, width, hover, onClick }: ButtonProps) => {
+  const state: ButtonState = isActive ? "default" : onClick ? "active" : "disabled"
   const baseStyle = "flex py-[0.8125rem] w-full text-body2 px-[1.375rem] justify-center items-center self-stretch rounded-[0.75rem] transition-colors";
-  const buttonStyle = useMemo(() => variant[state], [variant, state]);
+  const buttonStyle = useMemo(() => variantStyles[variant][state], [variant, state]);
 
   return (
     <button
       className={`${baseStyle} ${buttonStyle} ${width}`}
-      disabled={state === "disabled"}
       onClick={onClick}
     >
       {label}
