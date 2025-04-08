4c096f46948cfd7cbc4401d3a3a1f73f67cd01b3
bae080311 <s24024@gsm.hs.kr>
2025-04-08 19:02:54 +0900

    Fix: header design

--- apps/client/src/shared/ui/header/index.tsx
+++ apps/client/src/shared/ui/header/index.tsx
@@ -8,36 +8,38 @@ const Header = () => {
   const pathname = usePathname();
 
   return (
-    <header className="w-full py-[1.38rem] flex justify-around border-b">
-      <Link href="/">
-        <TextLogo />
-      </Link>
-      <ul className="gap-[2rem] flex text-label text-gray-500 items-center text-body3">
-        <li>
-          <Link
-            className={pathname === "/calculate" ? "text-black" : ""}
-            href="/calculate"
-          >
-            점수 계산
-          </Link>
-        </li>
-        <li>
-          <Link
-            className={pathname === "/example" ? "text-black" : ""}
-            href="/example"
-          >
-            예시
-          </Link>
-        </li>
-        <li>
-          <Link
-            className={pathname === "/posts" ? "text-black" : ""}
-            href="/posts"
-          >
-            내 글 목록
-          </Link>
-        </li>
-      </ul>
+    <header className="w-full py-[1.38rem] flex justify-center border-b">
+      <div className="w-[37.5rem] flex justify-between items-center">
+        <Link href="/">
+          <TextLogo />
+        </Link>
+        <ul className="gap-[2rem] flex text-label text-gray-500 items-center text-body3">
+          <li>
+            <Link
+              className={pathname === "/calculate" ? "text-black" : ""}
+              href="/calculate"
+            >
+              점수 계산
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={pathname === "/example" ? "text-black" : ""}
+              href="/example"
+            >
+              예시
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={pathname === "/posts" ? "text-black" : ""}
+              href="/posts"
+            >
+              내 글 목록
+            </Link>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 };
