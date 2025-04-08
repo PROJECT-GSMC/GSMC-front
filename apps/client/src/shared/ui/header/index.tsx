1332db681871910f840c9fcea2be5ab48f7c336e
bae080311 <s24024@gsm.hs.kr>
2025-04-08 14:20:56 +0900

    Feat: text color function

--- apps/client/src/shared/ui/header/index.tsx
+++ apps/client/src/shared/ui/header/index.tsx
@@ -1,16 +1,42 @@
+"use client";
+
 import TextLogo from "@repo/ui/textLogo";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+  const pathname = usePathname();
+
   return (
-    <header className="w-full py-[1.38rem] flex justify-around">
+    <header className="w-full py-[1.38rem] flex justify-around border-b">
       <Link href="/">
         <TextLogo />
       </Link>
       <ul className="gap-[2rem] flex text-label text-gray-500 items-center text-body3">
-        <Link href="/calculate">점수 계산</Link>
-        <Link href="/descrition">설명</Link>
-        <Link href="/posts">내 글 목록</Link>
+        <li>
+          <Link
+            className={pathname === "/calculate" ? "text-black" : ""}
+            href="/calculate"
+          >
+            점수 계산
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={pathname === "/example" ? "text-black" : ""}
+            href="/example"
+          >
+            예시
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={pathname === "/posts" ? "text-black" : ""}
+            href="/posts"
+          >
+            내 글 목록
+          </Link>
+        </li>
       </ul>
     </header>
   );
