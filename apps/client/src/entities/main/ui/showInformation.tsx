188c58194b0aa98480c395480762fa23f396f03f
bae080311 <s24024@gsm.hs.kr>
2025-04-09 19:51:41 +0900

    Feat: name and score ui

---
+++ apps/client/src/entities/main/ui/showInformation.tsx
@@ -0,0 +1,21 @@
+interface ShowInformationProps {
+  name: string;
+  score: number;
+}
+
+export const ShowInformation = ({ name, score }: ShowInformationProps) => {
+  return (
+    <div>
+      <div className="flex items-center gap-[0.75rem] mb-[1.69rem] mt-[3.87rem]">
+        <h3 className="text-titleMedium text-tropicalblue-700">{name}</h3>
+        <span className="text-titleSmall">님의 인증제 점수는</span>
+      </div>
+      <div className="flex items-center gap-[1.12rem]">
+        <h1 className="text-titleLarge px-[2.25rem] py-[0.75rem] text-tropicalblue-500 bg-gray-200 rounded-[6.25rem]">
+          {score}점
+        </h1>
+        <span className="text-titleSmall">입니다.</span>
+      </div>
+    </div>
+  );
+};
