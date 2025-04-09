6a546d310654bcbd4a15976bd72951a4234f7926
bae080311 <s24024@gsm.hs.kr>
2025-04-09 18:12:21 +0900

    Fix: header design

--- apps/client/src/shared/ui/header/index.tsx
+++ apps/client/src/shared/ui/header/index.tsx
@@ -9,35 +9,37 @@ const Header = () => {
 
   return (
     <header className="w-full py-[1.38rem] flex justify-around border-b">
-      <Link href="/">
-        <TextLogo />
-      </Link>
-      <ul className="gap-[2rem] flex text-label text-gray-500 items-center text-body3">
-        <li>
-          <Link
-            className={pathname === "/calculate" ? "text-black" : ""}
-            href="/calculate"
-          >
-            점수 계산
-          </Link>
-        </li>
-        <li>
-          <Link
-            className={pathname === "/example" ? "text-black" : ""}
-            href="/example"
-          >
-            예시
-          </Link>
-        </li>
-        <li>
-          <Link
-            className={pathname === "/posts" ? "text-black" : ""}
-            href="/posts"
-          >
-            내 글 목록
-          </Link>
-        </li>
-      </ul>
+      <div className="w-full max-w-[37.5rem] flex items-center justify-between">
+        <Link href="/">
+          <TextLogo />
+        </Link>
+        <ul className="gap-[2rem] flex text-label text-gray-500 items-center text-body3">
+          <li>
+            <Link
+              className={pathname === "/calculate" ? "text-black" : ""}
+              href="/calculate"
+            >
+              점수 계산
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={pathname === "/example" ? "text-black" : ""}
+              href="/example"
+            >
+              예시
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={pathname === "/posts" ? "text-black" : ""}
+              href="/posts"
+            >
+              내 글 목록
+            </Link>
+          </li>
+        </ul>
+      </div>
     </header>
   );
 };
