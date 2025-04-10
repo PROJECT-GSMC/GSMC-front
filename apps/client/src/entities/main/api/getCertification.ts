8ae06e2c6298db18d4d9f143fae997c98568a77f
bae080311 <s24024@gsm.hs.kr>
2025-04-10 23:21:53 +0900

    Feat: return type

--- apps/client/src/entities/main/api/getCertification.ts
+++ apps/client/src/entities/main/api/getCertification.ts
@@ -1,5 +1,6 @@
 import instance from "../../../../../../packages/ui/src/axios";
+import { Certification } from "../model/certification";
 
-export const getCertification = async () => {
+export const getCertification = async (): Promise<Certification[]> => {
   return instance.get("/certificates/current");
 };
