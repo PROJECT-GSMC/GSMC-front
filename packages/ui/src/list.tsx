f66f2b180b780a4217635ebe2ebf42f93170012a
bae080311 <s24024@gsm.hs.kr>
2025-04-10 22:22:49 +0900

    Fix: props

--- packages/ui/src/list.tsx
+++ packages/ui/src/list.tsx
@@ -1,31 +1,16 @@
-import Card from "@repo/ui/card";
-
 interface ListProps<T> {
-  values: T[];
+  children: React.ReactNode;
   title: string;
 }
 
-const List = <T,>({ values, title }: ListProps<T>) => {
+const List = <T,>({ children, title }: ListProps<T>) => {
   return (
     <div className="w-[573px] h-full mb-[2.5rem] bg-tropicalblue-100 py-[2.25rem] px-[1.38rem] rounded-[1.25rem]">
       <div className="flex items-center mb-[2.2rem]">
         <h4 className="text-tropicalblue-700 text-titleSmall">17</h4>
         <span className="text-body2">{title}</span>
       </div>
-      <div className="overflow-y-auto flex flex-col h-full">
-        <Card front="회원 목록" back="회원 목록" onClick={() => {}} />
-        <Card front="회원 목록" back="회원 목록" onClick={() => {}} />
-        <Card front="회원 목록" back="회원 목록" onClick={() => {}} />
-        <Card front="회원 목록" back="회원 목록" onClick={() => {}} />
-        {/* {member.map((item, index) => (
-          <Card
-            key={index}
-            front={JSON.stringify(item)}
-            back="회원 목록"
-            onClick={() => {}}
-          />
-        ))} */}
-      </div>
+      <div className="overflow-y-auto flex flex-col h-full">{children}</div>
     </div>
   );
 };
