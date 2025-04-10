555dc8229b0f658ad2b429b9dd615ea107d7feb7
bae080311 <s24024@gsm.hs.kr>
2025-04-10 22:16:58 +0900

    Fix: color

--- apps/client/src/entities/main/ui/showInformation.tsx
+++ apps/client/src/entities/main/ui/showInformation.tsx
@@ -11,7 +11,7 @@ export const ShowInformation = ({ name, score }: ShowInformationProps) => {
         <span className="text-titleSmall">님의 인증제 점수는</span>
       </div>
       <div className="flex items-center gap-[1.12rem]">
-        <h1 className="text-titleLarge px-[2.25rem] py-[0.75rem] text-tropicalblue-500 bg-gray-200 rounded-[6.25rem]">
+        <h1 className="text-titleLarge px-[2.25rem] py-[0.75rem] text-tropicalblue-500 bg-gray-100 rounded-[6.25rem]">
           {score}점
         </h1>
         <span className="text-titleSmall">입니다.</span>
