05ef5497700b88f17f2f9279f437b1455cb9c5af
bae080311 <s24024@gsm.hs.kr>
2025-04-10 23:03:49 +0900

    Fix: title

--- packages/ui/src/list.tsx
+++ packages/ui/src/list.tsx
@@ -7,8 +7,7 @@ const List = <T,>({ children, title }: ListProps<T>) => {
   return (
     <div className="w-[573px] h-full mb-[2.5rem] bg-tropicalblue-100 py-[2.25rem] px-[1.38rem] rounded-[1.25rem]">
       <div className="flex items-center mb-[2.2rem]">
-        <h4 className="text-tropicalblue-700 text-titleSmall">17</h4>
-        <span className="text-body2">{title}</span>
+        <h4 className="text-tropicalblue-700 text-titleSmall">{title}</h4>
       </div>
       <div className="overflow-y-auto flex flex-col h-full">{children}</div>
     </div>
