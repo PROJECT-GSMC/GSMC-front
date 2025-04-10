cc69b582ae20ca36a0f7f2ccf2955f288694b97b
bae080311 <s24024@gsm.hs.kr>
2025-04-10 23:21:04 +0900

    Feat: min-height

--- packages/ui/src/list.tsx
+++ packages/ui/src/list.tsx
@@ -5,7 +5,7 @@ interface ListProps<T> {
 
 const List = <T,>({ children, title }: ListProps<T>) => {
   return (
-    <div className="w-[573px] h-full mb-[2.5rem] bg-tropicalblue-100 py-[2.25rem] px-[1.38rem] rounded-[1.25rem]">
+    <div className="w-[573px] h-full min-h-[27.8125rem] mb-[2.5rem] bg-tropicalblue-100 py-[2.25rem] px-[1.38rem] rounded-[1.25rem]">
       <div className="flex items-center mb-[2.2rem]">
         <h4 className="text-tropicalblue-700 text-titleSmall">{title}</h4>
       </div>
