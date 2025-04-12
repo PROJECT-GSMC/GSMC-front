0ee274ceccb0cb3c2a18785e488de0357f824986
Dino0204 <s24020@gsm.hs.kr>
2025-04-12 18:52:10 +0900

    🐛 Fix: a -> link

--- apps/client/src/entities/main/ui/dropdown.tsx
+++ apps/client/src/entities/main/ui/dropdown.tsx
@@ -1,20 +1,22 @@
+import Link from "next/link"
+
 interface Dropdown {
   list: Array<DropdownItem>
 }
 
 interface DropdownItem {
   title: string
-  url: string
+  url?: string
 }
 
 const MainDropdown = ({ list }: Dropdown) => {
   return (
-    <div className="absolute left-0 top-full mt-2 z-10 flex flex-col gap-6 items-center justify-center rounded-xl w-[254px] px-11 py-6 bg-tropicalblue-50">
+    <div className="absolute left-0 top-full mt-2 z-10 flex flex-col gap-6 items-center justify-center rounded-xl w-[254px] px-11 py-6 bg-tropicalblue-50 ">
       {list.map((li) =>
-        <a className="w-full flex justify-between cursor-pointer" href={li.url}>
+        <Link className="w-full flex justify-between cursor-pointer" href={li.url || ""}>
           <p>{li.title}</p>
           <p>{">"}</p>
-        </a>
+        </Link>
       )}
     </div>
   );
