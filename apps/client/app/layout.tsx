0158ac3b62b2bdecc09f506fb733be0bd99f7a05
송재욱 <haensol976520@gmail.com>
2025-04-13 17:32:24 +0900

    feat: provider 설정

--- apps/client/app/layout.tsx
+++ apps/client/app/layout.tsx
@@ -1,19 +1,18 @@
 import "./globals.css";
 import type { Metadata } from "next";
+import Providers from "./providers";
 
 export const metadata: Metadata = {
   title: "GSMC",
   description: "GSM 인증제 작성 웹사이트입니다",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
