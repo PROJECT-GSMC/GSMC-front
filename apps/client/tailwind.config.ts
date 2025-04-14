d8e38f8a25f71043030bb113d72a5a35259b0256
송재욱 <haensol976520@gmail.com>
2025-04-14 16:38:29 +0900

    chore: 브레이크포인트 세팅

--- apps/client/tailwind.config.ts
+++ apps/client/tailwind.config.ts
@@ -1,8 +1,16 @@
 import type { Config } from "tailwindcss";
 import sharedConfig from "@repo/tailwind-config";
 
-const config: Pick<Config, "presets"> = {
+const config: Pick<Config, "presets" | "theme"> = {
   presets: [sharedConfig],
+  theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+    },
+  },
 };
 
 export default config;
