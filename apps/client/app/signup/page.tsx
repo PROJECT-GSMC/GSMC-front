fc2ff51476b2c31efdb3d623dd69d2adcbddb702
Dino0204 <s24020@gsm.hs.kr>
2025-04-15 00:42:26 +0900

    Feat: 회원 가입 페이지 퍼블리싱

--- apps/client/app/signup/page.tsx
+++ apps/client/app/signup/page.tsx
@@ -1,10 +1,12 @@
 "use client"
 import { Input } from "@repo/ui/input"
 import { Button } from "@repo/ui/button"
-import { useForm, Controller } from "react-hook-form"
+import { useForm } from "react-hook-form"
 import { AuthForm } from "../../src/widgets/auth/ui"
+import { AuthInput } from "@repo/ui/widgets/authInput/index"
 
 interface SignupFormProps {
+  name: string
   email: string;
   authcode: string;
 }
@@ -17,7 +19,7 @@ const SignupPage = () => {
       isValid,
       errors
     }
-  } = useForm<SignupFormProps>({ mode: "onChange", defaultValues: { email: '', authcode: '' } })
+  } = useForm<SignupFormProps>({ mode: "onChange", defaultValues: { name: '', email: '', authcode: '' } })
 
   const onSubmit = (data: SignupFormProps) => {
     alert(JSON.stringify(data))
@@ -31,28 +33,30 @@ const SignupPage = () => {
           className="flex flex-col items-center w-[25rem] gap-[3.625rem]"
         >
           <div className="flex flex-col gap-[0.75rem] self-stretch">
-            <Controller
-              name="email"
-              control={control}
-              rules={{
-                required: "이메일을 필수로 입력해야 합니다.",
-                pattern: {
-                  value: /^s\d{5}@gsm\.hs\.kr$/,
-                  message: "@gsm.hs.kr 학교 이메일을 입력해주세요"
-                }
-              }}
-              render={({ field }) => <Input label="이메일" type="email" error={errors.email?.message} {...field} />}
-            />
-            <Controller
-              name="authcode"
-              control={control}
-              rules={{
+            <AuthInput label="이름" error={errors.name?.message}>
+              <Input name="name" control={control} rules={{
+                required: "이름을 필수로 입력해야 합니다."
+              }} />
+            </AuthInput>
+            <AuthInput label="이메일" error={errors.email?.message}>
+              <div className="flex items-center justify-between gap-4">
+                <Input name="email" control={control} rules={{
+                  required: "이메일을 필수로 입력해야 합니다.",
+                  pattern: {
+                    value: /^s\d{5}@gsm\.hs\.kr$/,
+                    message: "@gsm.hs.kr 학교 이메일을 입력해주세요"
+                  }
+                }} />
+                <Button label="인증번호" variant="blue" />
+              </div>
+            </AuthInput>
+            <AuthInput label="인증번호" error={errors.authcode?.message}>
+              <Input name="authcode" control={control} rules={{
+                required: "인증번호를 필수로 입력해야 합니다.",
                 minLength: 8,
                 maxLength: 8,
-                required: "인증번호를 필수로 입력해야 합니다.",
-              }}
-              render={({ field }) => <Input label="인증번호" error={errors.authcode?.message} {...field} />}
-            />
+              }} />
+            </AuthInput>
           </div>
           <Button label="인증하기" variant="blue" isActive={isValid} />
         </form>
