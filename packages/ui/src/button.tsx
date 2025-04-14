4a8f43afeb7dd0c13d8dfaf8068efb8c1eb1f565
송재욱 <haensol976520@gmail.com>
2025-04-14 17:53:30 +0900

    feat: 버튼 컴포넌트 반응형

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -12,31 +12,14 @@ interface ButtonProps {
   onClick?: () => void;
 }
 
-export const Button = ({
-  variant,
-  isActive,
-  label,
-  width,
-  onClick,
-}: ButtonProps) => {
-  const state: ButtonState = isActive
-    ? "default"
-    : onClick
-      ? "active"
-      : "disabled";
+export const Button = ({ variant, isActive, label, width, onClick }: ButtonProps) => {
+  const state: ButtonState = isActive ? "default" : onClick ? "active" : "disabled";
   const baseStyle =
-    "flex py-[0.8125rem] w-full text-body2 px-[1.375rem] justify-center items-center self-stretch rounded-[0.75rem] transition-colors";
-  const buttonStyle = useMemo(
-    () => variantStyles[variant][state],
-    [variant, state]
-  );
+    "flex py-2 sm:py-[0.8125rem] w-full text-body2 px-3 sm:px-4 md:px-5 lg:px-[1.375rem] justify-center items-center self-stretch rounded-[0.75rem] transition-colors whitespace-nowrap";
+  const buttonStyle = useMemo(() => variantStyles[variant][state], [variant, state]);
 
   return (
-    <button
-      type="submit"
-      className={`${baseStyle} ${buttonStyle} ${width}`}
-      onClick={onClick}
-    >
+    <button type="submit" className={`${baseStyle} ${buttonStyle} ${width}`} onClick={onClick}>
       {label}
     </button>
   );
