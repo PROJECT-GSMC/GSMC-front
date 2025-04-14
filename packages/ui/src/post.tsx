acf351536a3f5821c8fe30ceb7b91b0f9dea9d33
bae080311 <s24024@gsm.hs.kr>
2025-04-14 09:58:39 +0900

    Feat: admin post style

--- packages/ui/src/post.tsx
+++ packages/ui/src/post.tsx
@@ -5,17 +5,17 @@ interface PostProps {
   isExample?: boolean;
 }
 
-const Post = ({ data, isExample }: PostProps) => {
+const Post = ({ data, isExample = true }: PostProps) => {
   const handleState = (state: string) => {
-    if (state === "APPROVE") return "승인됨";
-    if (state === "REJECT") return "거절됨";
-    if (state === "PENDING") return "대기중";
-    return "임시저장";
+    if (state === "APPROVE") return "통과";
+    if (state === "REJECT") return "탈락";
+    if (state === "PENDING") return "대기중..";
+    return "임시저장 글";
   };
   const handleStateColor = (state: string) => {
-    if (state === "APPROVE") return "text-green-500";
-    if (state === "REJECT") return "text-red-500";
-    if (state === "PENDING") return "text-yellow-500";
+    if (state === "APPROVE") return "text-tropicalblue-500";
+    if (state === "REJECT") return "text-[#DF454A]";
+    if (state === "PENDING") return "text-tropicalblue-800";
     return "text-gray-500";
   };
   return (
@@ -30,13 +30,22 @@ const Post = ({ data, isExample }: PostProps) => {
         <p className="text-gray-400 text-body5 mb-[0.75rem]">
           {data.categoryName}
         </p>
-        {isExample && (
-          <span
-            className={`text-body5 text-[${handleStateColor(data.status)}]`}
-          >
-            {handleState(data.status)}
-          </span>
-        )}
+        {isExample &&
+          (localStorage.getItem("role") === "ADMIN" &&
+          data.status === "PENDING" ? (
+            <div className="mt-[1.25rem] flex gap-[1rem] items-center justify-center text-body5 w-full text-white">
+              <button className=" bg-tropicalblue-500 px-[1.25rem] rounded-[0.5rem] py-[0.625rem]">
+                통과
+              </button>
+              <button className=" bg-[#DF454A] px-[1.25rem] rounded-[0.5rem] py-[0.625rem]">
+                거절
+              </button>
+            </div>
+          ) : (
+            <span className={`text-body5 ${handleStateColor(data.status)}`}>
+              {handleState(data.status)}
+            </span>
+          ))}
       </div>
     </div>
   );
