043e25483dd6b3e772d00c0ff680f1662c1ca882
bae080311 <s24024@gsm.hs.kr>
2025-04-15 21:11:10 +0900

    Fix: dropdown component props

--- apps/client/src/entities/main/ui/dropdown.tsx
+++ apps/client/src/entities/main/ui/dropdown.tsx
@@ -1,23 +1,14 @@
-import Link from "next/link";
+import React from "react";
 
 interface Dropdown {
-  list: Array<DropdownItem>;
+  children: React.ReactNode;
 }
-
-interface DropdownItem {
-  title: string;
-  url?: string;
-}
-
-const MainDropdown = ({ list }: Dropdown) => {
+const MainDropdown = ({ children }: Dropdown) => {
   return (
     <div className="absolute left-0 top-full mt-2 z-10 flex flex-col gap-4 sm:gap-6 items-center justify-center rounded-lg sm:rounded-xl w-[200px] m-4 sm:w-[254px] px-6 sm:px-11 py-4 sm:py-6 bg-tropicalblue-50">
-      {list.map((li) => (
-        <Link key={li.title} className="w-full flex justify-between cursor-pointer text-body3s " href={li.url || ""}>
-          <p>{li.title}</p>
-          <p>{">"}</p>
-        </Link>
-      ))}
+      <div className="w-full flex justify-between cursor-pointer text-body3s ">
+        {children}
+      </div>
     </div>
   );
 };
