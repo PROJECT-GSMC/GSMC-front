542102172564d15949953c450ae9741aef7a9ed4
Dino0204 <s24020@gsm.hs.kr>
2025-04-15 09:05:09 +0900

    🐛 Fix: Input generic type

--- packages/ui/src/input.tsx
+++ packages/ui/src/input.tsx
@@ -1,18 +1,22 @@
 "use client"
 import React from "react";
-import { useController, UseControllerProps } from "react-hook-form";
+import { useController, UseControllerProps, FieldValues } from "react-hook-form";
 
-interface InputProps extends UseControllerProps<any> {
+interface InputProps<T extends FieldValues = FieldValues> extends UseControllerProps<T> {
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  type?: string;
 }
 
-export const Input = ({ ...props }: InputProps) => {
+export const Input = <T extends FieldValues = FieldValues>({ type, ...props }: InputProps<T>) => {
   const { field } = useController(props);
+
+  // name이 password이면 password 타입으로, 아니면 주어진 type이나 기본값 text 사용
+  const inputType = field.name === "password" ? "password" : type || "text";
 
   return (
     <input
       className="px-[1rem] py-[0.75rem] rounded-[0.75rem] border focus: outline-tropicalblue-500 bg-white ui-outline-gray-600"
-      type={field.name}
+      type={inputType}
       {...field}
     />
   );
