159d8f9209b5195cf3089db7a9eeebc9ce4956cc
bae080311 <s24024@gsm.hs.kr>
2025-04-16 06:24:02 +0900

    Fix: evidence type

--- apps/client/src/widgets/main/model/evidence.ts
+++ apps/client/src/widgets/main/model/evidence.ts
@@ -1,6 +1,4 @@
-import { ACTIVITY_TYP } from "../../../../../../packages/ui/src/types/evidences";
-
 export interface Evidence {
   categoryName: string;
-  activityType: ACTIVITY_TYP;
+  file: File;
 }
