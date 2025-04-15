f0423761aa5a27834ffddb1cd30638b0cf8524ff
bae080311 <s24024@gsm.hs.kr>
2025-04-16 06:13:59 +0900

    Fix: dropdown style

--- apps/client/src/entities/main/ui/dropdown.tsx
+++ apps/client/src/entities/main/ui/dropdown.tsx
@@ -5,8 +5,8 @@ interface Dropdown {
 }
 const MainDropdown = ({ children }: Dropdown) => {
   return (
-    <div className="absolute left-0 top-full mt-2 z-10 flex flex-col gap-4 sm:gap-6 items-center justify-center rounded-lg sm:rounded-xl w-[200px] m-4 sm:w-[254px] px-6 sm:px-11 py-4 sm:py-6 bg-tropicalblue-50">
-      <div className="w-full flex justify-between cursor-pointer text-body3s ">
+    <div className="absolute left-0 z-10 flex flex-col gap-4 sm:gap-6 items-center justify-center rounded-lg sm:rounded-xl w-[200px] m-1 sm:w-[254px] px-6 sm:px-11 py-4 sm:py-6 bg-tropicalblue-50">
+      <div className="w-full flex gap-5 flex-col justify-between cursor-pointer text-body3s ">
         {children}
       </div>
     </div>
