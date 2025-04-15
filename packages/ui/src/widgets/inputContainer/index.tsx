3849d6eb63eb91ecdb7412a5cfee6cf856054075
Dino0204 <s24020@gsm.hs.kr>
2025-04-15 22:39:20 +0900

    🐛 Fix: 사용되지 않는 에러문 제거

--- packages/ui/src/widgets/inputContainer/index.tsx
+++ packages/ui/src/widgets/inputContainer/index.tsx
@@ -2,16 +2,14 @@ import React from "react";
 
 interface InputContainerProps {
   label: string
-  error?: string
   children: React.ReactNode
 }
 
-export const InputContainer = ({ label, error, children }: InputContainerProps) => {
+export const InputContainer = ({ label, children }: InputContainerProps) => {
   return (
     <div className="flex flex-col w-full gap-[0.56rem]">
-      <label htmlFor={label} className="text-label">{label}</label>
+      <label className="text-label">{label}</label>
       {children}
-      {error && <small role="alert" className="text-red-500">{error}</small>}
     </div>
   );
 };
