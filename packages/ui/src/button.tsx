73480e642720a86a20162683e294e48ce8f8061b
Dino0204 <s24020@gsm.hs.kr>
2025-04-15 23:33:37 +0900

    🐛 Fix: 버튼 컴포넌트 로직 변경

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -5,33 +5,24 @@ import { variantStyles } from "./consts/button";
 type ButtonState = "default" | "active" | "disabled";
 
 interface ButtonProps {
-  label: string | ReactNode;
   className?: string;
-  variant: keyof typeof variantStyles;
   width?: string;
-  isActive?: boolean;
+  label: string | ReactNode;
+  variant: keyof typeof variantStyles;
+  state: ButtonState
   onClick?: () => void;
 }
 
 export const Button = ({
-  variant,
-  isActive,
-  label,
   className,
   width,
+  label,
+  variant,
+  state,
   onClick,
 }: ButtonProps) => {
-  const state: ButtonState = isActive
-    ? "default"
-    : onClick
-      ? "active"
-      : "disabled";
-  const baseStyle =
-    "flex py-2 sm:py-[0.8125rem] w-full text-body2 px-3 sm:px-4 md:px-5 lg:px-[1.375rem] justify-center items-center self-stretch rounded-[0.75rem] transition-colors whitespace-nowrap";
-  const buttonStyle = useMemo(
-    () => variantStyles[variant][state],
-    [variant, state]
-  );
+  const baseStyle = "flex py-2 sm:py-[0.8125rem] w-full text-body2 px-3 sm:px-4 md:px-5 lg:px-[1.375rem] justify-center items-center self-stretch rounded-[0.75rem] transition-colors whitespace-nowrap";
+  const buttonStyle = useMemo(() => variantStyles[variant][state], [variant, state]);
 
   return (
     <button
