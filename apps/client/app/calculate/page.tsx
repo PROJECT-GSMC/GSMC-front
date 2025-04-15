a3c927fd63571a7901eedff43193b42d1ddf60b4
leesan <s24046@gsm.hs.kr>
2025-04-15 18:26:05 +0900

    Fix: style 수정

--- apps/client/app/calculate/page.tsx
+++ apps/client/app/calculate/page.tsx
@@ -5,7 +5,7 @@ const CalculatePage = () => {
   return (
     <div className="flex flex-col items-center justify-center">
       <Header />
-      <div className="flex flex-col gap-[3.06rem]">
+      <div className="flex flex-col gap-[3.06rem] mt-[1.88rem] max-w-[37.5rem] w-full">
         <h1 className="text-tropicalblue-700 text-titleMedium">점수 계산</h1>
         <Calculate />
       </div>
