9f1200eb93bd00b970e8fbb8658b4e26449cee7a
bae080311 <s24024@gsm.hs.kr>
2025-04-16 06:22:03 +0900

    Feat: transition styling

--- apps/client/src/entities/main/ui/dropdown.tsx
+++ apps/client/src/entities/main/ui/dropdown.tsx
@@ -1,14 +1,22 @@
 import React from "react";
 
-interface Dropdown {
+interface DropdownProps {
   children: React.ReactNode;
+  isOpen: boolean;
 }
-const MainDropdown = ({ children }: Dropdown) => {
+
+const MainDropdown = ({ children, isOpen }: DropdownProps) => {
   return (
-    <div className="absolute left-0 z-10 flex flex-col gap-4 sm:gap-6 items-center justify-center rounded-lg sm:rounded-xl w-[200px] m-1 sm:w-[254px] px-6 sm:px-11 py-4 sm:py-6 bg-tropicalblue-50">
-      <div className="w-full flex gap-5 flex-col justify-between cursor-pointer text-body3s ">
-        {children}
-      </div>
+    <div
+      className={`
+    absolute left-0 z-10 
+    w-[200px] sm:w-[254px] px-6 sm:px-11 py-4 sm:py-6 m-1 
+    rounded-lg sm:rounded-xl bg-tropicalblue-50
+    transition-all duration-500 ease-in-out origin-top 
+    ${isOpen ? "opacity-100 translate-y-0 pointer-events-auto" : "opacity-0 -translate-y-6 pointer-events-none"}
+  `}
+    >
+      <div className="w-full flex flex-col gap-5 text-body3s">{children}</div>
     </div>
   );
 };
