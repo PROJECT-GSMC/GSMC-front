482b54e03d4e22814d4cc10990fc909b80191f72
leesan <s24046@gsm.hs.kr>
2025-04-16 23:46:58 +0900

    Hotfix: calculate

---
+++ apps/client/src/widgets/calculate/model/category.ts
@@ -0,0 +1,116 @@
+export const majorCategoryOptions = [
+  {
+    name: "공문을 통한 전공분야 대회참여",
+    send: "major-out_school_attendance-official_contest",
+  },
+  {
+    name: "전공분야 대회 개별참여",
+    send: "major-out_school_attendace-inofficial_contest",
+  },
+  { name: "연합해커톤 참여", send: "major-out_school_attendance-hackathon" },
+  {
+    name: "전공관련교육프로그램 참여",
+    send: "major-out_school_attendance-seminar",
+  },
+  { name: "GSM FESTIVAL 참가", send: "major-in_school_attendance-gsmfest" },
+  { name: "교내해커톤대회 참가", send: "major-in_school_attendance-hackathon" },
+  {
+    name: "전공동아리 발표대회 참가",
+    send: "major-in_school_attendance-club_presentation",
+  },
+  { name: "전공특강(방과후) 참가", send: "major-in_school_attendance-seminar" },
+  {
+    name: "전공관련 방과후학교 이수",
+    send: "major-in_school_attendance-afterSchool",
+  },
+];
+
+export const humanCategoryOptions = [
+  {
+    name: "교내인성영역관련수상",
+    send: "humanities-award_career-humanity-in_school",
+  },
+  {
+    name: "교외인성영역관련수상",
+    send: "humanities-award_career-humanity-out_school",
+  },
+  {
+    name: "독서마라톤 거북이코스",
+    send: "humanities-book-bookmarathon-turtle",
+  },
+  {
+    name: "독서마라톤 악어코스",
+    send: "humanities-book-bookmarathon-crocodile",
+  },
+  {
+    name: "독서마라톤 토끼코스이상",
+    send: "humanities-book-bookmarathon-over_rabbit",
+  },
+  {
+    name: "학교지정도서 권장",
+    send: "humanities-book-school_designated_book",
+  },
+  {
+    name: "봉사활동",
+    send: "humanities-service-activity",
+  },
+  {
+    name: "봉사동아리",
+    send: "humanities-service-club",
+  },
+  {
+    name: "한자자격증",
+    send: "humanities-certificate-chinese_character",
+  },
+  {
+    name: "한국사",
+    send: "humanities-certificate-korean_history",
+  },
+  {
+    name: "자기주도적 활동",
+    send: "humanities-activities-self_directed_activities",
+  },
+  {
+    name: "뉴로우S",
+    send: "humanities-activities-newrrow_s",
+  },
+];
+
+export const foreignCategoryOptions = [
+  {
+    name: "TOEIC 점수",
+    send: "foreign_lang-test-toeic-score",
+  },
+  {
+    name: " TOEFL 점수",
+    send: "foreign_lang-test-toefl-score",
+  },
+  {
+    name: "TEPS 점수",
+    send: "foreign_lang-test-teps-score",
+  },
+  {
+    name: "TOEIC SPEAKING level",
+    send: "foreign_lang-test-toeic_speaking-level",
+  },
+  {
+    name: "OPIC 등급",
+    send: "foreign_lang-test-opic-grade",
+  },
+  {
+    name: "JPT 점수",
+    send: "foreign_lang-test-jpt-score",
+  },
+  {
+    name: "CPT 점수 ",
+    send: "foreign_lang-test-cpt-score",
+  },
+  {
+    name: "HSK 급수",
+    send: "foreign_lang-test-hsk-grade",
+  },
+  {
+    name: "토사관 참여",
+    send: "foreign_lang-attendance_toeic_academy-status",
+  },
+];
