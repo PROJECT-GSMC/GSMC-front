2c3087256823eb77e6300750670ded57574e23e0
bae080311 <s24024@gsm.hs.kr>
2025-04-16 23:19:27 +0900

    Feat: use get member

---
+++ apps/admin/src/widget/member/model/useGetMember.ts
@@ -0,0 +1,17 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { getMembers } from "../api/getMembers";
+import { toast } from "sonner";
+
+export const useGetMember = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: getMembers,
+    onSuccess: (data) => {
+      queryClient.setQueryData(["members"], data.data);
+    },
+    onError: (e) => {
+      toast.error(e.message);
+    },
+  });
+};
