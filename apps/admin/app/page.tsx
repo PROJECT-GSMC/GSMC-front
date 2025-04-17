24cdf2a993d42297f067fc0cf33d26790a8d29d6
bae080311 <s24024@gsm.hs.kr>
2025-04-17 17:23:58 +0900

    Feat: 학생이 선택되지 않은 경우

--- apps/admin/app/page.tsx
+++ apps/admin/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Header from "../src/shared/ui/header";
 import Filter from "../src/widget/member/ui/filter";
 import { Information } from "../src/widget/member/ui/information";
@@ -5,34 +7,59 @@ import { useGetMember } from "../src/widget/member/model/useGetMember";
 import { Member } from "../src/widget/member/model/member";
 import { toast } from "sonner";
 import Card from "@repo/ui/card";
+import { useState } from "react";
+import { getMember } from "../src/widget/member/api/getMember";
+import List from "@repo/ui/list";
+import Question from "../src/shared/asset/svg/question";
 
 const MemberPage = () => {
+  const [student, setStudent] = useState<Member>();
+  const [grade, setGrade] = useState<number>(0);
+  const [classNumber, setClassNumber] = useState<number>(0);
+
   const { data, error } = useGetMember();
-
   if (error) toast.error(error.message);
-
   const members: Member[] = data?.data;
   return (
-    <div style={{ gap: "1rem" }} className="flex overflow-hidden flex-col">
+    <div
+      style={{ gap: "1rem" }}
+      className="flex overflow-hidden h-full flex-col"
+    >
       <Header />
       <div
-        className="flex justify-center items-center flex-wrap"
+        className="flex justify-center items-center w-full h-full mt-[3.12rem]"
         style={{ gap: "1.8rem" }}
       >
-        {members.map((members) => {
-          return (
-            <Card
-              key={members.email}
-              front={members.name}
-              back={
-                members.grade +
-                members.classNumber +
-                String(members.number).padStart(2, "0")
-              }
-            />
-          );
-        })}
-        <Information />
+        <List className="h-[46rem]" title={members && String(members.length)}>
+          {members &&
+            members.map((members) => {
+              return (
+                <Card
+                  onClick={async () => {
+                    const res = await getMember(members.email);
+                    setStudent(res.data);
+                  }}
+                  key={members.email}
+                  front={members.name}
+                  back={
+                    members.grade +
+                    members.classNumber +
+                    String(members.number).padStart(2, "0")
+                  }
+                />
+              );
+            })}
+        </List>
+        {student ? (
+          <Information student={student} />
+        ) : (
+          <div className="w-[350px] min-w-[18rem] bg-tropicalblue-100 py-[2.25rem] flex flex-col justify-center items-center rounded-[1.25rem] h-[46rem] px-[2.45rem]">
+            <Question />
+            <span className="text-titleSmall text-gray-600">
+              학생을 선택해주세요
+            </span>
+          </div>
+        )}
         <Filter />
       </div>
     </div>
