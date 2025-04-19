63b5729d1d09c4fedf582d605cbd026ec99e837e
bae080311 <s24024@gsm.hs.kr>
2025-04-19 18:08:28 +0900

    Fix: param type

--- apps/admin/src/entities/check-post/api/changeEvidenceState.ts
+++ apps/admin/src/entities/check-post/api/changeEvidenceState.ts
@@ -2,7 +2,7 @@ import instance from "../../../../../../packages/ui/src/axios";
 import { postState } from "../../../../../../packages/ui/src/types/evidences";
 
 export const changeEvidenceState = async (
-  evidenceId: string,
+  evidenceId: number,
   status: postState
 ) => {
   instance.patch(`/evidence/${evidenceId}/status`, status);
