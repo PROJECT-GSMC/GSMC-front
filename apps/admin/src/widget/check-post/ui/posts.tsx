cdb12d9321fa048dd44ed5f20844ec2b67bab589
bae080311 <s24024@gsm.hs.kr>
2025-04-19 18:00:16 +0900

    Feat: check post page

---
+++ apps/admin/src/widget/check-post/ui/posts.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { Button } from "@repo/ui/button";
+import { useGetPosts } from "../model/useGetPosts";
+import { useState } from "react";
+import { postState } from "../../../../../../packages/ui/src/types/evidences";
+import Header from "../../../shared/ui/header";
+import Post from "../../../entities/check-post/ui/post";
+import { PostType } from "../../../../../client/src/entities/posts/model/postType";
+import { toast } from "sonner";
+
+const PostsWidget = () => {
+  const [state, setState] = useState<postState | "">("");
+  const { isError } = useGetPosts();
+  if (isError) {
+    toast.error("게시글을 불러오지 못했습니다.");
+  }
+  const { data } = useGetPosts();
+  const filteredPosts = data?.filter((post: PostType) => {
+    post.status === state;
+  });
+  return (
+    <div className="flex items-center flex-col">
+      <Header />
+      <div className="w-full max-w-[37.5rem]">
+        <h1 className="text-tropicalblue-700 text-h4s mb-[2.06rem] mt-[2.94rem]">
+          모태환님의 글
+        </h1>
+        <div className="flex gap-[5%] justify-between">
+          <Button
+            onClick={() => setState("APPROVE")}
+            variant={state === "APPROVE" ? "blue" : "skyblue"}
+            label="통과"
+            isActive
+          />
+          <Button
+            onClick={() => setState("REJECT")}
+            variant={state === "REJECT" ? "blue" : "skyblue"}
+            isActive
+            label="거절"
+          />
+          <Button
+            onClick={() => setState("PENDING")}
+            variant={state === "PENDING" ? "blue" : "skyblue"}
+            isActive
+            label="대기"
+          />
+        </div>
+        <div className="flex flex-wrap">
+          {filteredPosts &&
+            filteredPosts.map((post) => {
+              return <Post key={post.id} data={post} />;
+            })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PostsWidget;
