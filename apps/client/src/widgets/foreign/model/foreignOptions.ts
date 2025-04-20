63c9146b76612f7dde93745d1b0b7d0841db3395
bae080311 <s24024@gsm.hs.kr>
2025-04-20 15:24:23 +0900

    Feat: options

---
+++ apps/client/src/widgets/foreign/model/foreignOptions.ts
@@ -0,0 +1,10 @@
+export const foreignOptions = [
+  { name: "TOEIC", send: "TOEIC" },
+  { name: "TOEFL", send: "TOEFL" },
+  { name: "TEPS", send: "TEPS" },
+  { name: "TOEIC_SPEAKING", send: "TOEIC_SPEAKING" },
+  { name: "OPIC", send: "OPIC" },
+  { name: "JPT", send: "JPT" },
+  { name: "CPT", send: "CPT" },
+  { name: "HSK", send: "HSK" },
+];
