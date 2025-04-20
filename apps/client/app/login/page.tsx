8d838643d01d8ecd4d4e77ce0c91a9892d02795d
송재욱 <haensol976520@gmail.com>
2025-04-20 15:50:05 +0900

    feat: 로그인 버튼 색 수정

--- apps/client/app/login/page.tsx
+++ apps/client/app/login/page.tsx
@@ -56,7 +56,7 @@ const LoginPage = () => {
               />
             </InputContainer>
           </div>
-          <Button label="로그인" variant="blue" state={isValid ? "active" : "disabled"} />
+          <Button label="로그인" variant="blue" state={isValid ? "default" : "disabled"} />
         </form>
       </AuthForm>
     </div>
