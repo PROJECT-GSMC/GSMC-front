5f9996edd36e1bad68cf340543d9400b294b417d
bae080311 <s24024@gsm.hs.kr>
2025-04-22 02:31:07 +0900

    Feat: deal form

--- apps/admin/src/widget/score/ui/scoreForm.tsx
+++ apps/admin/src/widget/score/ui/scoreForm.tsx
@@ -6,33 +6,47 @@ import { Controller, useForm } from "react-hook-form";
 import Checkbox from "../../../entities/score/ui/checkbox";
 import Header from "../../../shared/ui/header";
 import { InputContainer } from "@repo/ui/widgets/inputContainer/index";
+import { ScoreFormType } from "../model/score";
+import { featScore } from "../api/featScore";
+import { useParams } from "next/navigation";
 
 const ScoreForm = () => {
-  const { control } = useForm();
+  const P = useParams<{ email: string }>();
+  const email = P.email;
+
+  const {
+    handleSubmit,
+    control,
+    formState: { isValid },
+  } = useForm<ScoreFormType>({ mode: "onChange" });
+
   return (
     <div className="w-full flex flex-col items-center justify-center">
       <Header />
-      <div className="flex flex-col gap-[2rem] w-[600px]">
+      <form
+        onSubmit={handleSubmit((data) => {
+          featScore(email, "humanities-service-activity", data.oneSemester);
+          featScore(email, "humanities-service-activity", data.twoSemester);
+          featScore(email, "humanities-activities-newrrow_s", data.newrow);
+          featScore(
+            email,
+            "foreign_lang-attendance_toeic_academy-status",
+            data.checkbox ? 1 : 0
+          );
+        })}
+        className="flex flex-col gap-[2rem] w-[600px]"
+      >
         <h1 className="text-titleMedium text-tropicalblue-700 mt-[2.38rem]">
           점수 추가
         </h1>
         <InputContainer label="1학기 봉사 시간">
-          <Input
-            control={control}
-            name="oneSemester"
-          />
+          <Input control={control} name="oneSemester" />
         </InputContainer>
         <InputContainer label="2학기 봉사 시간">
-          <Input
-            control={control}
-            name="twoSemester"
-          />
+          <Input control={control} name="twoSemester" />
         </InputContainer>
         <InputContainer label="뉴로우 참여 횟수">
-          <Input
-            control={control}
-            name="newrow"
-          />
+          <Input control={control} name="newrow" />
         </InputContainer>
         <Controller
           control={control}
@@ -40,10 +54,14 @@ const ScoreForm = () => {
           render={({ field }) => <Checkbox {...field} />}
         />
         <div className="flex flex-col gap-[0.69rem]">
-          <Button variant="skyblue" label="뒤로가기" isActive />
-          <Button variant="blue" label="점수 주기 완료" isActive />
+          <Button variant="skyblue" label="뒤로가기" />
+          <Button
+            state={isValid ? "default" : "disabled"}
+            variant="blue"
+            label="점수 주기 완료"
+          />
         </div>
-      </div>
+      </form>
     </div>
   );
 };
