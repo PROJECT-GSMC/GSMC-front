54f0e31df3cc6efac903c7e422a65e635727bee0
Dino0204 <s24020@gsm.hs.kr>
2025-04-22 20:30:39 +0900

    🐛 Fix: Post 이미지 너비 문제

--- apps/client/src/shared/ui/post/post.tsx
+++ apps/client/src/shared/ui/post/post.tsx
@@ -1,27 +1,29 @@
-import { PostType } from "../../../../../../packages/ui/src/types/postType";
 import Image from "next/image";
 import {
   handleState,
   handleStateColor,
 } from "../../../../../../packages/ui/src/utils/handleState";
+import { Others, Reading, Activity } from "../../../../../../packages/ui/src/types/evidences";
+import { isActivity, isReading, isOthers } from "../../../../../../packages/ui/src/utils/handlePost";
 
 interface PostProps {
-  data: PostType;
+  data: Activity | Reading | Others;
   isExample?: boolean;
 }
 
 const Post = ({ data, isExample = false }: PostProps) => {
   return (
     <div className="flex flex-col w-[188px] rounded-[0.625rem] h-[276px]">
-      {data.imageUrl ? (
-        <Image className="h-[150px]" src={data.imageUrl} alt={data.title} />
-      ) : (
-        <div className="bg-gray-400 w-full h-[150px] rounded-t-[0.625rem]"></div>
-      )}
+      <div className="bg-gray-400 w-full h-[150px] rounded-t-[0.625rem]">
+        {isActivity(data) && data.imageUrl && (
+          <Image src={data.imageUrl} alt={data.title} className="object-cover" fill />
+        )}
+      </div>
+
       <div className="px-[0.38rem] py-[0.75rem]">
-        <h3 className="text-body2 mb-[1rem]">{data.title}</h3>
+        {isActivity(data) || isReading(data) && <h3 className="text-body2 mb-[1rem]">{data.title}</h3>}
         <p className="text-gray-400 text-body5 mb-[0.75rem]">
-          {data.categoryName}
+          {(isActivity(data) || isOthers(data) && data.categoryName) ?? (isReading(data) && data.author)}
         </p>
         {!isExample && (
           <span className={`text-body5 ${handleStateColor(data.status)}`}>
