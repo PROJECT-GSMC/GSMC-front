6a8c508e2378cfc825eb7cdd3d8de70006069f9f
Dino0204 <s24020@gsm.hs.kr>
2025-04-22 20:31:03 +0900

    🐛 Fix: type guard

--- apps/client/app/detail/[id]/page.tsx
+++ apps/client/app/detail/[id]/page.tsx
@@ -1,37 +1,44 @@
 import { Button } from "@repo/ui/button";
-import { PostType } from "../../../../../packages/ui/src/types/postType"
 import Header from "../../../src/shared/ui/header";
 import Image from "next/image";
+import { Activity, Others, Reading } from "../../../../../packages/ui/src/types/evidences";
+import { isActivity, isOthers, isReading } from "../../../../../packages/ui/src/utils/handlePost";
 
 interface DetailProps {
-  params: {
-    id: string
-  }
-  data: PostType
+  data: Activity | Reading | Others
 }
 
-
-const DetailPage = ({ params, data }: DetailProps) => {
+const DetailPage = ({ data }: DetailProps) => {
   return (
     <>
       <Header />
       <div className="flex flex-col items-center mt-[3rem]">
         <div className="flex flex-col w-[37.5rem] gap-[1.75rem]">
           <header className="flex flex-col w-full gap-[0.5rem]">
-            <h1 className="text-[2.25rem] font-semibold">{data?.title ?? "Title"}</h1>
-            <h3 className="text-[0.75rem] text-[#767676] text-right font-normal">{"Author"} . {data?.categoryName ?? "Area"}</h3>
+            <h1 className="text-[2.25rem] font-semibold">
+              {(isActivity(data) || isReading(data)) ? data.title : "Title"}
+            </h1>
+            <h3 className="text-[0.75rem] text-[#767676] text-right font-normal">
+              {(isReading(data) && data.author) ? data.author : "Author"}
+              {" . "}
+              {(isActivity(data) || isOthers(data)) ? data.categoryName : "Area"}
+            </h3>
             <div className="w-full h-[0.5px] bg-[#A6A6A6]"></div>
           </header>
 
           <main className="flex flex-col gap-[3rem] ">
-            {data?.imageUrl &&
+            {isActivity(data) && data.imageUrl && (
               <div className="h-[21.215rem] bg-slate-600">
-                <Image src={data?.imageUrl} alt="img" className="w-full" />
+                <Image src={data.imageUrl} alt={data.title ?? "img"} fill className="w-full" />
               </div>
-            }
+            )}
             <section className="flex flex-col gap-[1rem]">
-              <h2 className="text-[1.5rem] font-semibold">{"Category: "}</h2>
-              <p className="text-[1.25rem] font-normal min-h-[29.9375rem]">{data?.content}</p>
+              <h2 className="text-[1.5rem] font-semibold">
+                카테고리: {(isActivity(data) || isOthers(data)) ? data.categoryName : (isReading(data) ? "독서" : "")}
+              </h2>
+              <p className="text-[1.25rem] font-normal min-h-[29.9375rem]">
+                {(isActivity(data) || isReading(data)) ? data.content : ""}
+              </p>
             </section>
           </main>
 
@@ -39,11 +46,11 @@ const DetailPage = ({ params, data }: DetailProps) => {
             <Button label="수정하기" variant="blue" className="w-full" />
             <Button label="뒤로가기" variant="skyblue" className="w-full bg-white" />
           </footer>
-
         </div>
       </div>
     </>
   );
 };
+
 
 export default DetailPage;
