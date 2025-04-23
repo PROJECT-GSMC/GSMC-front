2d24c75b03aaf047f325b4f47bae69e562a26675
bae080311 <s24024@gsm.hs.kr>
2025-04-23 14:05:52 +0900

    Remove: semester

--- apps/client/src/widgets/book/ui/index.tsx
+++ apps/client/src/widgets/book/ui/index.tsx
@@ -20,22 +20,22 @@ const BookWidget = () => {
   return (
     <div className="flex flex-col items-center">
       <Header />
-      <div
-        className="px-4  justify-center w-full max-w-[37.5rem]"
-        onSubmit={handleSubmit(async (data) => {
-          try {
-            await sendBook(data);
-            toast.success("글 제출을 성공했습니다");
-          } catch (e) {
-            toast.error("글 제출을 실패했습니다");
-            console.error(e);
-          }
-        })}
-      >
+      <div className="px-4  justify-center w-full max-w-[37.5rem]">
         <h1 className="text-tropicalblue-700 text-title4s my-[1rem] sm:my-[2.38rem] mb-6 sm:text-titleMedium">
           독서 영역
         </h1>
-        <form className="flex gap-[2rem] flex-col">
+        <form
+          onSubmit={handleSubmit(async (data) => {
+            try {
+              await sendBook(data);
+              toast.success("글 제출을 성공했습니다");
+            } catch (e) {
+              toast.error("글 제출을 실패했습니다");
+              console.error(e);
+            }
+          })}
+          className="flex gap-[2rem] flex-col"
+        >
           <InputContainer label="제목">
             <Input
               name="title"
@@ -66,15 +66,6 @@ const BookWidget = () => {
               }}
             />
           </InputContainer>
-          <Controller
-            name="semester"
-            defaultValue={0}
-            control={control}
-            rules={{
-              required: "학기를 선택해주세요.",
-            }}
-            render={({ field }) => <Semester {...field} />}
-          />
           <Controller
             name="content"
             defaultValue=""
