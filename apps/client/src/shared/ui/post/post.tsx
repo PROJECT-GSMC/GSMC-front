144396831a8696fe22af650593b285202a4c3b7e
bae080311 <s24024@gsm.hs.kr>
2025-04-23 12:28:26 +0900

    Fix: image style

--- apps/client/src/shared/ui/post/post.tsx
+++ apps/client/src/shared/ui/post/post.tsx
@@ -3,8 +3,16 @@ import {
   handleState,
   handleStateColor,
 } from "../../../../../../packages/ui/src/utils/handleState";
-import { Others, Reading, Activity } from "../../../../../../packages/ui/src/types/evidences";
-import { isActivity, isReading, isOthers } from "../../../../../../packages/ui/src/utils/handlePost";
+import {
+  Others,
+  Reading,
+  Activity,
+} from "../../../../../../packages/ui/src/types/evidences";
+import {
+  isActivity,
+  isReading,
+  isOthers,
+} from "../../../../../../packages/ui/src/utils/handlePost";
 
 interface PostProps {
   data: Activity | Reading | Others;
@@ -16,14 +24,25 @@ const Post = ({ data, isExample = false }: PostProps) => {
     <div className="flex flex-col w-[188px] rounded-[0.625rem] h-[276px]">
       <div className="bg-gray-400 w-full h-[150px] rounded-t-[0.625rem]">
         {isActivity(data) && data.imageUrl && (
-          <Image src={data.imageUrl} alt={data.title} className="object-cover" fill />
+          <Image
+            src={data.imageUrl}
+            width={188}
+            height={170}
+            alt={data.title}
+            className="object-cover w-[188px] rounded-t-[0.625rem] h-[150px]"
+            style={{ objectFit: "cover" }}
+          />
         )}
       </div>
 
       <div className="px-[0.38rem] py-[0.75rem]">
-        {isActivity(data) || isReading(data) && <h3 className="text-body2 mb-[1rem]">{data.title}</h3>}
+        {isActivity(data) ||
+          (isReading(data) && (
+            <h3 className="text-body2 mb-[1rem]">{data.title}</h3>
+          ))}
         <p className="text-gray-400 text-body5 mb-[0.75rem]">
-          {(isActivity(data) || isOthers(data) && data.categoryName) ?? (isReading(data) && data.author)}
+          {(isActivity(data) || (isOthers(data) && data.categoryName)) ??
+            (isReading(data) && data.author)}
         </p>
         {!isExample && (
           <span className={`text-body5 ${handleStateColor(data.status)}`}>
