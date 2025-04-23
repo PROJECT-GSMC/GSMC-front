90670d0656b1e96a11556bbed5b0963b37309159
bae080311 <s24024@gsm.hs.kr>
2025-04-23 12:17:21 +0900

    Fix: gap

--- apps/client/src/widgets/major/ui/index.tsx
+++ apps/client/src/widgets/major/ui/index.tsx
@@ -43,67 +43,69 @@ const MajorWidget = () => {
   return (
     <div className="flex flex-col items-center">
       <Header />
-      <form
-        className="flex sm:gap-[2rem] gap-[1.5rem] flex-col px-4 justify-center w-full max-w-[37.5rem]"
-        onSubmit={handleSubmit(onSubmit)}
-      >
+      <form className=" flex-col px-4 justify-center w-full max-w-[37.5rem]">
         <h1 className="text-tropicalblue-700 text-title4s sm:text-titleMedium my-[1rem] sm:my-[2.38rem]">
           전공 영역
         </h1>
-        <Controller
-          name="categoryName"
-          control={control}
-          rules={{
-            required: "카테고리를 선택해주세요.",
-          }}
-          render={({ field }) => (
-            <Dropdown
-              label="카테고리"
-              options={majorCategoryOptions}
-              {...field}
-            />
-          )}
-        />
-        <InputContainer label="주제">
-          <Input
-            name="title"
-            defaultValue=""
+        <form
+          className="flex sm:gap-[2rem] gap-[1.5rem] flex-col"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <Controller
+            name="categoryName"
             control={control}
             rules={{
-              required: "주제를 입력해주세요.",
+              required: "카테고리를 선택해주세요.",
             }}
+            render={({ field }) => (
+              <Dropdown
+                label="카테고리"
+                options={majorCategoryOptions}
+                {...field}
+              />
+            )}
           />
-        </InputContainer>
-        <Controller
-          name="content"
-          control={control}
-          rules={{
-            required: "내용을 입력해주세요.",
-            minLength: {
-              value: file ? 200 : 400,
-              message: file
-                ? "내용을 200자 이상 입력해주세요."
-                : "내용을 400자 이상 입력해주세요.",
-            },
-          }}
-          render={({ field }) => <Textarea {...field} />}
-        />
-        <Controller
-          name="file"
-          control={control}
-          rules={{
-            required: "파일을 선택해주세요.",
-          }}
-          render={({ field }) => <File label="이미지" {...field} />}
-        />
-        <div className="w-full flex flex-col gap-[0.69rem] text-[0.875rem] mb-[2rem] mt-[4rem]">
-          <Button variant="skyblue" label="임시저장" />
-          <Button
-            state={isValid ? "default" : "disabled"}
-            variant="blue"
-            label="작성 완료"
+          <InputContainer label="주제">
+            <Input
+              name="title"
+              defaultValue=""
+              control={control}
+              rules={{
+                required: "주제를 입력해주세요.",
+              }}
+            />
+          </InputContainer>
+          <Controller
+            name="content"
+            control={control}
+            rules={{
+              required: "내용을 입력해주세요.",
+              minLength: {
+                value: file ? 200 : 400,
+                message: file
+                  ? "내용을 200자 이상 입력해주세요."
+                  : "내용을 400자 이상 입력해주세요.",
+              },
+            }}
+            render={({ field }) => <Textarea {...field} />}
           />
-        </div>
+          <Controller
+            name="file"
+            control={control}
+            rules={{
+              required: "파일을 선택해주세요.",
+            }}
+            render={({ field }) => <File label="이미지" {...field} />}
+          />
+          <div className="w-full flex flex-col gap-[0.69rem] text-[0.875rem] mb-[2rem] mt-[4rem]">
+            <Button variant="skyblue" label="임시저장" />
+            <Button
+              state={isValid ? "default" : "disabled"}
+              variant="blue"
+              label="작성 완료"
+            />
+          </div>
+        </form>
       </form>
     </div>
   );
