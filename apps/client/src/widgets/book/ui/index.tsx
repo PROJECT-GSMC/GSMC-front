aa4c48cdace93c26ace43071064c8d941d3522b6
bae080311 <s24024@gsm.hs.kr>
2025-04-23 14:05:15 +0900

    Fix: styling

--- apps/client/src/widgets/book/ui/index.tsx
+++ apps/client/src/widgets/book/ui/index.tsx
@@ -20,8 +20,8 @@ const BookWidget = () => {
   return (
     <div className="flex flex-col items-center">
       <Header />
-      <form
-        className="flex gap-[2rem] flex-col justify-center w-full max-w-[37.5rem]"
+      <div
+        className="px-4  justify-center w-full max-w-[37.5rem]"
         onSubmit={handleSubmit(async (data) => {
           try {
             await sendBook(data);
@@ -32,63 +32,69 @@ const BookWidget = () => {
           }
         })}
       >
-        <h1 className="text-tropicalblue-700 text-titleMedium my-[2.38rem]">
+        <h1 className="text-tropicalblue-700 text-title4s my-[1rem] sm:my-[2.38rem] mb-6 sm:text-titleMedium">
           독서 영역
         </h1>
-        <InputContainer label="제목" >
-          <Input
-            name="title"
-            control={control}
-            defaultValue=""
-            rules={{
-              required: "제목을 입력해주세요.",
-            }}
-          />
-        </InputContainer>
-        <InputContainer label="저자">
-          <Input
-            name="author"
-            defaultValue=""
-            control={control}
-            rules={{
-              required: "저자를 입력해주세요.",
-            }}
-          />
-        </InputContainer>
-        <InputContainer label="페이지">
-          <Input
-            name="page"
-            control={control}
+        <form className="flex gap-[2rem] flex-col">
+          <InputContainer label="제목">
+            <Input
+              name="title"
+              control={control}
+              defaultValue=""
+              rules={{
+                required: "제목을 입력해주세요.",
+              }}
+            />
+          </InputContainer>
+          <InputContainer label="저자">
+            <Input
+              name="author"
+              defaultValue=""
+              control={control}
+              rules={{
+                required: "저자를 입력해주세요.",
+              }}
+            />
+          </InputContainer>
+          <InputContainer label="페이지">
+            <Input
+              name="page"
+              control={control}
+              defaultValue={0}
+              rules={{
+                required: "페이지를 입력해주세요.",
+              }}
+            />
+          </InputContainer>
+          <Controller
+            name="semester"
             defaultValue={0}
+            control={control}
             rules={{
-              required: "페이지를 입력해주세요.",
+              required: "학기를 선택해주세요.",
             }}
+            render={({ field }) => <Semester {...field} />}
           />
-        </InputContainer>
-        <Controller
-          name="semester"
-          defaultValue={0}
-          control={control}
-          rules={{
-            required: "학기를 선택해주세요.",
-          }}
-          render={({ field }) => <Semester {...field} />}
-        />
-        <Controller
-          name="content"
-          defaultValue=""
-          control={control}
-          rules={{
-            required: "내용을 입력해주세요.",
-          }}
-          render={({ field }) => <Textarea isBook {...field} />}
-        />
+          <Controller
+            name="content"
+            defaultValue=""
+            control={control}
+            rules={{
+              required: "내용을 입력해주세요.",
+            }}
+            render={({ field }) => <Textarea isBook {...field} />}
+          />
 
-        <div className="w-full flex flex-col gap-[0.69rem] text-[0.875rem] mb-[2rem] mt-[4rem]">
-          <Button variant="skyblue" label="임시저장" />
-          <Button state={isValid ? "default" : "disabled"} variant="blue" label="작성 완료" />
-        </div>
-      </form>
+          <div className="w-full flex flex-col gap-[0.69rem] text-[0.875rem] mb-[2rem] mt-[4rem]">
+            <Button variant="skyblue" label="임시저장" />
+            <Button
+              state={isValid ? "default" : "disabled"}
+              variant="blue"
+              label="작성 완료"
+            />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
