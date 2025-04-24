c5fb6ee8b11cad81c50130d6a59a6151e957958b
Dino0204 <s24020@gsm.hs.kr>
2025-04-24 17:35:52 +0900

    🎨 Improve: 반응형 적용

--- apps/client/src/widgets/auth/ui/index.tsx
+++ apps/client/src/widgets/auth/ui/index.tsx
@@ -7,7 +7,7 @@ interface AuthForm {
 
 export const AuthForm = ({ children, label }: AuthForm) => {
   return (
-    <div className="flex flex-col pt-[5.625rem] p-[6.25rem] justify-center items-center rounded-[1.25rem] bg-white ">
+    <div className="flex flex-col p-[2.25rem] pt-[2.62rem] md:p-[4.25rem] md:pt-[4.62rem] lg:p-[6.25rem] lg:pt-[5.62rem] justify-center items-center rounded-[1.25rem] bg-white ">
       <h1 className="text-[2.25rem] font-bold">{label}</h1>
       {children}
     </div>
