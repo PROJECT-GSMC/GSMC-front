68073d8c1615839cbb2b3ab3360bcddce665aac5
bae080311 <s24024@gsm.hs.kr>
2025-04-24 15:30:09 +0900

    Feat: await

--- apps/client/src/widgets/main/api/sendCertification.ts
+++ apps/client/src/widgets/main/api/sendCertification.ts
@@ -2,7 +2,7 @@ import instance from "../../../../../../packages/ui/src/axios";
 import { Certification } from "../model/certification";
 
 export const sendCertification = async (data: Certification) => {
-  return instance.post("/certificates", {
+  return await instance.post("/certificates", {
     headers: {
       "Content-Type": "multipart/form-data",
     },
--- apps/client/src/widgets/main/api/sendEvidence.ts
+++ apps/client/src/widgets/main/api/sendEvidence.ts
@@ -2,7 +2,7 @@ import instance from "../../../../../../packages/ui/src/axios";
 import { Evidence } from "../model/evidence";
 
 export const sendEvidence = async (data: Evidence) => {
-  return instance.post("/evidence/current/other", {
+  return await instance.post("/evidence/current/other", {
     headers: {
       "Content-Type": "multipart/form-data",
     },
