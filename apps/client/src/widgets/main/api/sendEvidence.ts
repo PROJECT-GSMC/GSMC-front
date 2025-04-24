0c89ba3614de3a2359a9d7065a5eb9268d2685b4
bae080311 <s24024@gsm.hs.kr>
2025-04-24 16:48:15 +0900

    Fix: send evidence request

--- apps/client/src/widgets/main/api/sendEvidence.ts
+++ apps/client/src/widgets/main/api/sendEvidence.ts
@@ -2,6 +2,9 @@ import FormInstance from "../../../../../../packages/ui/src/formAxios";
 import { Evidence } from "../model/evidence";
 
 export const sendEvidence = async (data: Evidence) => {
+  const formData = new FormData();
+  formData.append("file", data.file);
+  formData.append("categoryName", data.categoryName);
   return await FormInstance.post("/evidence/current/other", {
     ...data,
   });
