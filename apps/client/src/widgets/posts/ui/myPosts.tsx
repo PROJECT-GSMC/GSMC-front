1dff87fff7574041dae1228803c758c69f6276db
xy-jxn <s24016@gsm.hs.kr>
2025-04-25 01:12:02 +0900

    fix: 내 글 보기 페이지 반응형 작업

--- apps/client/src/widgets/posts/ui/myPosts.tsx
+++ apps/client/src/widgets/posts/ui/myPosts.tsx
@@ -11,14 +11,14 @@ const PostsWidget = () => {
   return (
     <div className="flex flex-col items-center">
       <Header />
-      <div className="w-full max-w-[37.5rem]">
+      <div className="w-full max-w-[37.5rem] px-[1rem] sm:px-[0rem] min-w-[17.9rem] overflow-auto">
         <Search result={result} setResult={setResult} />
-        <div className="flex gap-[1rem] justify-between">
-          <Button label="독서" variant="skyblue" />
-          <Button label="인성" variant="skyblue" />
-          <Button label="전공" variant="skyblue" />
-          <Button label="외국어" variant="skyblue" />
-          <Button label="임시저장" variant="skyblue" />
+        <div className="grid justify-items-center sm:grid-cols-5 grid-cols-3 gap-[1rem]">
+          <Button label="독서" variant="skyblue" className="min-w-[4.3rem]" />
+          <Button label="인성" variant="skyblue" className="min-w-[4.3rem]" />
+          <Button label="전공" variant="skyblue" className="min-w-[4.3rem]" />
+          <Button label="외국어" variant="skyblue" className="min-w-[4.3rem]" />
+          <Button label="임시저장" variant="skyblue" className="min-w-[4.3rem]" />
         </div>
         <div className="flex mt-[2.69rem] overflow-y-visible flex-wrap w-full justify-center gap-[1.12rem]">
           <Post
@@ -26,7 +26,7 @@ const PostsWidget = () => {
               id: 1,
               title: "예시 게시물 제목",
               content: "이것은 게시물의 내용입니다.",
-              imageUrl: "https://via.placeholder.com/150",
+              imageUrl: "",
               status: "APPROVE",
               categoryName: "동아리",
             }}
@@ -36,7 +36,7 @@ const PostsWidget = () => {
               id: 1,
               title: "예시 게시물 제목",
               content: "이것은 게시물의 내용입니다.",
-              imageUrl: "https://via.placeholder.com/150",
+              imageUrl: "",
               status: "APPROVE",
               categoryName: "동아리",
             }}
