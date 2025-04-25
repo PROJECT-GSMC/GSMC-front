e5984af900c22d756bad97f7092366e4cb73ab4d
xy-jxn <s24016@gsm.hs.kr>
2025-04-25 11:42:29 +0900

    fix: 예시 페이지 Title 반응형 추가

--- apps/client/app/example/page.tsx
+++ apps/client/app/example/page.tsx
@@ -10,7 +10,7 @@ const ExamplePage = () => {
     <div className="flex flex-col items-center">
       <Header />
       <div className="w-full max-w-[37.5rem] px-[1rem] sm:px-[0rem] min-w-[17.9rem] overflow-auto">
-        <h1 className="text-tropicalblue-700 text-titleMedium sm:text-left text-center my-[2.38rem]">
+        <h1 className="text-tropicalblue-700 text-title4s sm:text-titleMedium sm:text-left text-center my-[2.38rem]">
           쓰기 예시 페이지
         </h1>
         <div className="flex mt-[2.69rem] overflow-y-visible flex-wrap sm:justify-start justify-center w-full gap-[1.12rem]">
