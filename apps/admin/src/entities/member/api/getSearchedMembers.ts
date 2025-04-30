b19eb71564fce95637408cf41b981c6c07cd67d4
Dino0204 <s24020@gsm.hs.kr>
2025-04-30 15:09:13 +0900

    feat: getSearchedMembers

---
+++ apps/admin/src/entities/member/api/getSearchedMembers.ts
@@ -0,0 +1,23 @@
+import instance from "../../../../../../packages/ui/src/axios";
+
+import { SearchParams } from "../model/search";
+
+export const getSearchedMembers = async ({
+  grade,
+  classNumber,
+  name,
+  page = 1,
+  size = 10,
+}: SearchParams) => {
+  const query: Record<string, string> = {};
+
+  if (grade != null) query.grade = String(grade);
+  if (classNumber != null) query.classNumber = String(classNumber);
+  if (name != null) query.name = String(name);
+  if (page != null) query.page = String(page);
+  if (size != null) query.size = String(size);
+
+  const queryString = new URLSearchParams(query).toString();
+
+  return instance.get(`/students/search?${queryString}`);
+};
