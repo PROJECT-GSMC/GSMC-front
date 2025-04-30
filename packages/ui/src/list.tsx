f86d9ecd83e9a065557c42beed7a02cec6260ac6
Dino0204 <s24020@gsm.hs.kr>
2025-04-30 15:10:00 +0900

    Chore: list

--- packages/ui/src/list.tsx
+++ packages/ui/src/list.tsx
@@ -11,12 +11,12 @@ interface ListProps {
 
 const List = ({ children, title, className, onClick }: ListProps) => {
   return (
-    <div className="max-w-[37.5rem] h-full min-h-[27.8125rem] px-[2.25rem] max-h-[46.1875rem] py-[3.5rem] bg-tropicalblue-100 rounded-[1.25rem]">
+    <div className="h-full min-h-[27.8125rem] max-h-[46.1875rem] px-[2.25rem] py-[3.5rem] bg-tropicalblue-100 rounded-[1.25rem]">
       <div className="flex justify-between items-center">
-        <h4 className={`text-tropicalblue-700 ${className} text-titleSmall`}>
+        <h4 className={`${className} text-tropicalblue-700 text-titleSmall`}>
           {title}
         </h4>
-        <span className="md:hidden cursor-pointer" onClick={onClick}>
+        <span className="cursor-pointer" onClick={onClick}>
           <Filtered />
         </span>
       </div>
