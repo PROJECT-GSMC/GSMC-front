1649ae66aaeeda48ed2873b731cc4997d936e0db
bae080311 <s24024@gsm.hs.kr>
2025-05-03 05:30:16 +0900

    Feat: refresh logic

--- apps/client/app/middleware.ts
+++ apps/client/app/middleware.ts
@@ -13,25 +13,69 @@ const Protected = [
 const allAllowed = ["/", "/signup", "/signin"];
 
 function isProtectedRoute(path: string): boolean {
-  if (Protected.includes(path)) {
-    return true;
-  }
   return Protected.some((route) => path.startsWith(route));
 }
 
-export function middleware(request: NextRequest) {
-  const pathname = request.nextUrl.pathname;
+async function refreshAccessToken(refreshToken: string) {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}auth/refresh`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ refreshToken }),
+      }
+    );
 
+    if (!response.ok) return null;
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+export async function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
   const isAllowed = allAllowed.includes(pathname);
 
   const token = request.cookies.get("accessToken")?.value || null;
-  const isTokenValid = Boolean(token);
+  const refreshToken = request.cookies.get("refreshToken")?.value || null;
 
-  if (isProtectedRoute(pathname) && !isTokenValid) {
-    return NextResponse.redirect(new URL("/", request.url));
+  if (isProtectedRoute(pathname)) {
+    if (!token && refreshToken) {
+      const res = await refreshAccessToken(refreshToken);
+      if (res) {
+        const response = NextResponse.next();
+        response.cookies.set("accessToken", res.accessToken, {
+          path: "/",
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: "lax",
+          maxAge: 60 * 60 * 24,
+        });
+        response.cookies.set("refreshToken", res.refreshToken, {
+          path: "/",
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          sameSite: "lax",
+          maxAge: 60 * 60 * 24 * 7,
+        });
+        return response;
+      }
+      return NextResponse.redirect(new URL("/signin", request.url));
+    }
+
+    if (!token) {
+      return NextResponse.redirect(new URL("/signin", request.url));
+    }
   }
 
-  if (isTokenValid && isAllowed) {
+  if (token && isAllowed) {
     const redirectUrl = new URL("/", request.url);
     return NextResponse.redirect(redirectUrl);
   }
