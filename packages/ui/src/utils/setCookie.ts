4796c55565ad5f27ca3ca08be3de4ba57b274b3a
bae080311 <s24024@gsm.hs.kr>
2025-05-03 05:48:06 +0900

    Feat: set cookie

---
+++ packages/ui/src/utils/setCookie.ts
@@ -0,0 +1,12 @@
+export const setCookie = (name: string, value: string, days = 7): void => {
+  if (typeof document === "undefined") return;
+
+  try {
+    const expires = new Date();
+    expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+    (document as unknown as { cookie: string }).cookie =
+      `${name}=${value};expires=${expires.toUTCString()};path=/`;
+  } catch (error) {
+    console.error(error);
+  }
+};
