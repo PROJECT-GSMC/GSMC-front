0104117782c65f3047a3cca182faadeeefd0be11
bae080311 <s24024@gsm.hs.kr>
2025-05-03 04:27:46 +0900

    Feat: set token

---
+++ apps/client/src/shared/lib/setAuthCookies.ts
@@ -0,0 +1,20 @@
+import { cookies } from "next/headers";
+
+export async function setAuthCookies(
+  accessToken: string,
+  refreshToken: string
+) {
+  const cookieStore = await cookies();
+
+  cookieStore.set("accessToken", accessToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "lax",
+  });
+
+  cookieStore.set("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "lax",
+  });
+}
