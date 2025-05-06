ce2ce4b7b6693f8216b308e0fa6c46b45fb5eda8
Dino0204 <s24020@gsm.hs.kr>
2025-05-06 19:24:55 +0900

    feat: list token filter optional

--- packages/ui/src/list.tsx
+++ packages/ui/src/list.tsx
@@ -6,19 +6,22 @@ interface ListProps {
   children: React.ReactNode;
   title: string;
   className?: string;
+  accessToken?: string;
   onClick?: () => void;
 }
 
-const List = ({ children, title, className, onClick }: ListProps) => {
+const List = ({ children, title, className, accessToken, onClick }: ListProps) => {
   return (
     <div className="h-full px-[2.25rem] py-[3.5rem] bg-tropicalblue-100 rounded-[1.25rem]">
       <div className="flex justify-between items-center">
         <h4 className={`${className} text-tropicalblue-700 text-titleSmall`}>
           {title}
         </h4>
-        <span className="cursor-pointer" onClick={onClick}>
-          <Filtered />
-        </span>
+        {accessToken ?
+          (<span className="cursor-pointer" onClick={onClick}>
+            <Filtered />
+          </span>) : (null)
+        }
       </div>
       <div className="overflow-y-auto flex flex-col h-full">{children}</div>
     </div>
