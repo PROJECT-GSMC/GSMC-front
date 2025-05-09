8fe8e35e80f007ed55f8288dad977ef78b51c891
bae080311 <s24024@gsm.hs.kr>
2025-05-09 09:16:02 +0900

    fix: instance

--- apps/client/src/widgets/main/api/sendCertification.ts
+++ apps/client/src/widgets/main/api/sendCertification.ts
@@ -1,10 +1,10 @@
-import FormInstance from "node_modules/@repo/ui/src/formAxios";
 import { Certification } from "../model/certification";
+import instance from "node_modules/@repo/ui/src/axios";
 
 export const sendCertification = async (data: Certification) => {
   const formData = new FormData();
   formData.append("file", data.file);
   formData.append("name", data.name);
   formData.append("acquisitionDate", data.acquisitionDate);
-  return await FormInstance.post("/certificates", formData);
+  return await instance.post("certificates", formData);
 };
