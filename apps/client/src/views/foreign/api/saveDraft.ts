8b70e224f27ecdd4791b55ed11bac5a7deaebb2e
bae080311 <s24024@gsm.hs.kr>
2025-05-10 23:53:43 +0900

    remove: import

--- apps/client/src/views/foreign/api/saveDraft.ts
+++ apps/client/src/views/foreign/api/saveDraft.ts
@@ -1,4 +1,3 @@
-import { Activity } from "@/shared/types/activity";
 import instance from "node_modules/@repo/ui/src/axios";
 
 export const saveDraft = async (activity: FormData) => {
--- apps/client/src/views/posts/api/getPosts.ts
+++ apps/client/src/views/posts/api/getPosts.ts
@@ -1,4 +1,3 @@
-import { Evidence } from "@/widgets/main/model/evidence";
 import instance from "node_modules/@repo/ui/src/axios";
 import { EvidenceType } from "node_modules/@repo/ui/src/types/evidences";
 
