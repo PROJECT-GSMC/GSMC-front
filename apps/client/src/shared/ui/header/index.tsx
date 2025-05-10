136a409aff0799acade96e96187be55bd354b91a
Dino0204 <s24020@gsm.hs.kr>
2025-05-11 02:23:37 +0900

    feat: header 로그아웃 기능

--- apps/client/src/shared/ui/header/index.tsx
+++ apps/client/src/shared/ui/header/index.tsx
@@ -1,11 +1,27 @@
 "use client";
 
-import TextLogo from "@repo/ui/textLogo";
-import Link from "next/link";
+
+import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
+import Link from "next/link";
+
+import TextLogo from "@repo/ui/textLogo";
+import { getCookie } from "node_modules/@repo/ui/src/utils/getCookie";
+import { deleteCookie } from "node_modules/@repo/ui/src/utils/deleteCookie";
 
 const Header = () => {
   const pathname = usePathname();
+  const [accessToken, setAccessToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    const token = getCookie("accessToken");
+    setAccessToken(token);
+  }, []);
+
+  const deleteToken = () => {
+    deleteCookie("accessToken")
+    deleteCookie("refreshToken")
+  }
 
   return (
     <header className="w-full py-[1.38rem] flex justify-around border-b px-4">
@@ -38,6 +54,18 @@ const Header = () => {
               내 글 목록
             </Link>
           </li>
+          <li
+            className="hover:text-black cursor-pointer"
+          >
+            비밀번호 변경
+          </li>
+          {accessToken &&
+            <li
+              className="hover:text-black cursor-pointer"
+              onClick={() => deleteToken()}
+            >
+              로그아웃
+            </li>}
         </ul>
       </div>
     </header>
