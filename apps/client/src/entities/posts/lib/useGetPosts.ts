36d118a028ce1ee012d6d283f2b7e339e738c390
bae080311 <s24024@gsm.hs.kr>
2025-05-10 21:53:25 +0900

    fix: evidence type

--- apps/client/src/entities/posts/lib/useGetPosts.ts
+++ apps/client/src/entities/posts/lib/useGetPosts.ts
@@ -1,7 +1,8 @@
 import { getPosts } from "@/views/posts/api/getPosts";
 import { useQuery } from "@tanstack/react-query";
+import { EvidenceType } from "node_modules/@repo/ui/src/types/evidences";
 
-export const useGetPosts = ({ type }: { type: string }) => {
+export const useGetPosts = ({ type }: { type: EvidenceType }) => {
   return useQuery({
     queryKey: ["posts"],
     queryFn: () => getPosts(type),
--- apps/client/src/views/posts/api/getPosts.ts
+++ apps/client/src/views/posts/api/getPosts.ts
@@ -1,5 +1,7 @@
+import { Evidence } from "@/widgets/main/model/evidence";
 import instance from "node_modules/@repo/ui/src/axios";
+import { EvidenceType } from "node_modules/@repo/ui/src/types/evidences";
 
-export const getPosts = async (type: string) => {
-  return await instance.get(`/evidence/current`);
+export const getPosts = async (type: EvidenceType) => {
+  return await instance.get(`/evidence/current?type=${type}`);
 };
