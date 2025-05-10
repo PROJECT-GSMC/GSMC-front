850997429190d918b5a390087cb5ff8ab6a9e2ce
bae080311 <s24024@gsm.hs.kr>
2025-05-10 21:51:27 +0900

    fix: end point

--- apps/client/src/views/posts/api/getDraft.ts
+++ apps/client/src/views/posts/api/getDraft.ts
@@ -1,5 +1,5 @@
 import instance from "node_modules/@repo/ui/src/axios";
 
 export const getDraft = async () => {
-  return await instance.get("/current/draft");
+  return await instance.get("/evidence/current/draft");
 };
