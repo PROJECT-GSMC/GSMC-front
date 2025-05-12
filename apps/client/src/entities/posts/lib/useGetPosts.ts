c7b884124678f1fa65e8796290187f7328218b46
bae080311 <s24024@gsm.hs.kr>
2025-05-12 22:50:01 +0900

    feat: key type

--- apps/client/src/entities/posts/lib/useGetPosts.ts
+++ apps/client/src/entities/posts/lib/useGetPosts.ts
@@ -2,9 +2,9 @@ import { getPosts } from "@/views/posts/api/getPosts";
 import { useQuery } from "@tanstack/react-query";
 import { EvidenceType } from "node_modules/@repo/ui/src/types/evidences";
 
-export const useGetPosts = ({ type }: { type: EvidenceType }) => {
+export const useGetPosts = (type: EvidenceType) => {
   return useQuery({
-    queryKey: ["posts"],
+    queryKey: ["posts", type],
     queryFn: () => getPosts(type),
   });
 };
