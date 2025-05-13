9b0c96b0050dadc9eb1bf8ac961e686d083eb6bc
bae080311 <s24024@gsm.hs.kr>
2025-05-13 23:28:33 +0900

    fix: type

--- apps/client/src/entities/posts/lib/useGetPosts.ts
+++ apps/client/src/entities/posts/lib/useGetPosts.ts
@@ -2,7 +2,7 @@ import { getPosts } from "@/views/posts/api/getPosts";
 import { useQuery } from "@tanstack/react-query";
 import { EvidenceType } from "node_modules/@repo/ui/src/types/evidences";
 
-export const useGetPosts = (type: EvidenceType) => {
+export const useGetPosts = (type: EvidenceType | null) => {
   return useQuery({
     queryKey: ["posts", type],
     queryFn: () => getPosts(type),
