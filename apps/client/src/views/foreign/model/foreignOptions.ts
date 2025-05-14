1b187a53e6c56df02ae1569f6140e25b737fa53e
bae080311 <s24024@gsm.hs.kr>
2025-05-14 14:47:33 +0900

    fix: foreign option

--- apps/client/src/views/foreign/model/foreignOptions.ts
+++ apps/client/src/views/foreign/model/foreignOptions.ts
@@ -1,13 +1,13 @@
 export const foreignOptions = [
-  { name: "TOEIC", send: "foreign_lang-test-toeic-score" },
-  { name: "TOEFL", send: "foreign_lang-test-toefl-score" },
-  { name: "TEPS", send: "foreign_lang-test-teps-score" },
+  { name: "TOEIC", send: "FOREIGN_LANG-TOEIC_SCORE" },
+  { name: "TOEFL", send: "FOREIGN_LANG-TOEFL_SCORE" },
+  { name: "TEPS", send: "FOREIGN_LANG-TEPS_SCORE" },
   {
     name: "TOEIC SPEAKING",
-    send: "foreign_lang-test-toeic_speaking-level",
+    send: "FOREIGN_LANG-TOEIC_SPEAKING_LEVEL",
   },
-  { name: "OPIC", send: "foreign_lang-test-opic-grade" },
-  { name: "JPT", send: "foreign_lang-test-jpt-score" },
-  { name: "CPT", send: "foreign_lang-test-cpt-score" },
-  { name: "HSK", send: "foreign_lang-test-hsk-grade" },
+  { name: "OPIC", send: "FOREIGN_LANG-OPIC_SCORE" },
+  { name: "JPT", send: "FOREIGN_LANG-JPT_SCORE" },
+  { name: "CPT", send: "FOREIGN_LANG-CPT_SCORE" },
+  { name: "HSK", send: "FOREIGN_LANG-HSK_SCORE" },
 ];
