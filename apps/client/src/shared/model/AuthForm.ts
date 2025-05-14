e8afbd2e601fc8f0dde3e4ab8e644e3969df7cab
Dino0204 <s24020@gsm.hs.kr>
2025-05-14 10:11:22 +0900

    fix: step 별 회원가입 인터페이스 분리

--- apps/client/src/shared/model/AuthForm.ts
+++ apps/client/src/shared/model/AuthForm.ts
@@ -3,8 +3,19 @@ export interface SigninFormProps {
   password: string;
 }
 
-export interface SignupFormProps extends SigninFormProps {
+export interface SignupFormProps {
+  email: string;
+  password: string;
   name: string;
+}
+
+export interface AuthStepForm {
+  email: string;
   authcode: string;
+  name: string;
+}
+
+export interface SignupStepForm {
+  password: string;
   passwordCheck: string;
 }
