65d4a27b33e9cd89b00b86c211c6cfecc2885228
bae080311 <s24024@gsm.hs.kr>
2025-05-17 00:39:47 +0900

    fix: get member request

--- apps/admin/src/widgets/member/api/getMember.ts
+++ apps/admin/src/widgets/member/api/getMember.ts
@@ -1,5 +1,5 @@
 import instance from "@repo/api/axios";
 
 export const getMember = async (email: string) => {
-  return instance.get(`/students/${email}`);
+  return instance.get(`/members/students/${email.split("@")[0]}`);
 };
