f7f9c405d3403231e85c65638ab219d1e5558a4d
bae080311 <s24024@gsm.hs.kr>
2025-05-18 23:19:27 +0900

    fix: handle email

--- apps/admin/src/views/check-post/api/getPosts.ts
+++ apps/admin/src/views/check-post/api/getPosts.ts
@@ -2,5 +2,7 @@ import instance from "@repo/api/axios";
 import { postState } from "@repo/types/evidences";
 
 export const getPosts = async (email: string, status: postState | null) => {
-  return await instance.get(`/evidence/${email}?status=${status}`);
+  return await instance.get(
+    `/evidence/${email.split("@")[0]?.substring(1)}?status=${status}`
+  );
 };
