b1ecc9410357aa5be5c7d61749c2e003e78cc658
bae080311 <s24024@gsm.hs.kr>
2025-05-19 00:26:26 +0900

    feat: return type

--- apps/admin/src/entities/check-post/model/useGetStudent.ts
+++ apps/admin/src/entities/check-post/model/useGetStudent.ts
@@ -1,9 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
 import { getStudent } from "../api/getStudent";
+import { Member } from "@repo/types/member";
 
-export const useGetPosts = (email: string) => {
-  return useQuery({
-    queryKey: ["posts", email],
+export const useGetStudent = (email: string) => {
+  return useQuery<Member>({
+    queryKey: ["student", email],
     queryFn: () => getStudent(email),
     staleTime: 1000 * 60 * 5,
     gcTime: 1000 * 60 * 30,
