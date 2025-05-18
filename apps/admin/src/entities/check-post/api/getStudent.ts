8fc5924bfbbeaef977dab01a16effd05416c04b3
bae080311 <s24024@gsm.hs.kr>
2025-05-19 00:33:28 +0900

    fix: request

--- apps/admin/src/entities/check-post/api/getStudent.ts
+++ apps/admin/src/entities/check-post/api/getStudent.ts
@@ -1,5 +1,7 @@
 import instance from "@repo/api/axios";
 
 export const getStudent = async (email: string) => {
-  return await instance.get(`/student/${email.split("@")[0]?.substring(1)}`);
+  return await instance.get(
+    `members/students/${email.split("@")[0]?.substring(1)}`
+  );
 };
