b8acaee54274d065d49db158ea06d5ce8e7474f6
bae080311 <s24024@gsm.hs.kr>
2025-05-19 23:14:53 +0900

    feat: return 값 명시

--- apps/client/src/entities/posts/lib/useGetDraft.ts
+++ apps/client/src/entities/posts/lib/useGetDraft.ts
@@ -1,8 +1,9 @@
 import { getDraft } from "@/views/posts/api/getDraft";
+import { post } from "@repo/types/evidences";
 import { useQuery } from "@tanstack/react-query";
 
 export const useGetDraft = () => {
-  return useQuery({
+  return useQuery<post[]>({
     queryKey: ["drafts"],
     queryFn: getDraft,
   });
