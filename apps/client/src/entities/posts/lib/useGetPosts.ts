6d2bafe0fa9a63c1d672ffb907c4997fa311a1af
bae080311 <s24024@gsm.hs.kr>
2025-05-19 23:15:37 +0900

    feat: 임시저장, 버튼 컬러

--- apps/client/src/entities/posts/lib/useGetPosts.ts
+++ apps/client/src/entities/posts/lib/useGetPosts.ts
@@ -2,9 +2,10 @@ import { getPosts } from "@/views/posts/api/getPosts";
 import { useQuery } from "@tanstack/react-query";
 import { EvidenceType } from "@repo/types/evidences";
 
-export const useGetPosts = (type: EvidenceType | null) => {
+export const useGetPosts = (type: EvidenceType | "DRAFT" | null) => {
   return useQuery({
     queryKey: ["posts", type],
-    queryFn: () => getPosts(type),
+    queryFn: () => getPosts(type as EvidenceType | null),
+    enabled: type !== "DRAFT",
   });
 };
