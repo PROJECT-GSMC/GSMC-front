529571d058d1b33dc6af4c91ddc88c14ca2ca868
재욱 <haensol976520@gmail.com>
2025-05-27 10:39:04 +0900

    feat: 경로 수정

--- apps/client/src/shared/api/updateOthers.ts
+++ apps/client/src/shared/api/updateOthers.ts
@@ -1,5 +1,5 @@
 import instance from "@repo/api/axios";
 
 export const updateOthers = async (evidenceId: number, formData: FormData) => {
-  await instance.patch(`evidence/current/other/${evidenceId}`, formData);
+  await instance.patch(`evidence/other/${evidenceId}`, formData);
 }; 
--- apps/client/src/shared/api/updateReading.ts
+++ apps/client/src/shared/api/updateReading.ts
@@ -2,5 +2,5 @@ import instance from "@repo/api/axios";
 import { Book } from "@/views/book/model/book";
 
 export const updateReading = async (evidenceId: number, bookData: Book) => {
-  await instance.patch(`evidence/current/reading/${evidenceId}`, bookData);
+  await instance.patch(`evidence/reading/${evidenceId}`, bookData);
 }; 
