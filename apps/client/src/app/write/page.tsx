695f984a1c30f04a7d2ccb9f46c64a0b489e65ba
bae080311 <s24024@gsm.hs.kr>
2025-05-29 14:34:47 +0900

    fix: build error

--- apps/client/src/app/write/page.tsx
+++ apps/client/src/app/write/page.tsx
@@ -1,5 +1,10 @@
 import WriteView from "@/views/write/ui";
+import { Suspense } from "react";
 
 export default function WritePage() {
-  return <WriteView />;
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <WriteView />
+    </Suspense>
+  );
 }
