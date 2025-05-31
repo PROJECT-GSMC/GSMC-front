00c881b348ed171c8f33d79a0531c7527bce7b30
재욱 <haensol976520@gmail.com>
2025-05-31 20:55:46 +0900

    fix: 선택 영역 처리 로직 개선

--- packages/shared/src/list.tsx
+++ packages/shared/src/list.tsx
@@ -1,6 +1,8 @@
 "use client"
 
-const Filtered = require("./filtered");
+import type React from "react";
+
+import { Filtered } from "./filtered.js";
 
 interface ListProps {
   children: React.ReactNode;
@@ -10,18 +12,24 @@ interface ListProps {
   onClick?: () => void;
 }
 
-const List = ({ children, title, className, isFilter = false, onClick }: ListProps) => {
+const List = ({
+  children,
+  title,
+  className,
+  isFilter = false,
+  onClick,
+}: ListProps) => {
   return (
     <div className="h-full px-[2.25rem] py-[3.5rem] bg-tropicalblue-100 rounded-[1.25rem]">
       <div className="flex justify-between items-center">
-        <h4 className={`${className} text-tropicalblue-700 text-titleSmall`}>
+        <h4 className={`${className ?? ""} text-tropicalblue-700 text-titleSmall`}>
           {title}
         </h4>
-        {isFilter ?
-          (<span className="cursor-pointer" onClick={onClick}>
+        {isFilter ? (
+          <span className="cursor-pointer" onClick={onClick}>
             <Filtered />
-          </span>) : (null)
-        }
+          </span>
+        ) : null}
       </div>
       <div className="overflow-y-auto flex flex-col h-full">{children}</div>
     </div>
