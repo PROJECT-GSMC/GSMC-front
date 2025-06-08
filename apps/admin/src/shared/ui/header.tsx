7f620f34fa2deacb58dd78d5a522843dd9667dcc
bae080311 <s24024@gsm.hs.kr>
2025-06-08 17:55:17 +0900

    fix: order

--- apps/admin/src/shared/ui/header.tsx
+++ apps/admin/src/shared/ui/header.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const Header = () => {
   return (
     <header className="sm:py-[1.4rem] py-[1rem] flex w-full justify-center items-center border-b">
-      <Link href="/" className="flex items-center">
+      <Link className="flex items-center" href="/">
         <TextLogo />
       </Link>
     </header>
