14dc0e3e21833af2b59595c086e0aba76681cd49
bae080311 <s24024@gsm.hs.kr>
2025-06-09 16:07:41 +0900

    fix: type

--- apps/admin/src/widgets/member/model/useGetMember.ts
+++ apps/admin/src/widgets/member/model/useGetMember.ts
@@ -1,9 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
+import type { AxiosResponse } from "axios";
+import type { Member } from "@repo/types/member";
 
 import { getMembers } from "../api/getMembers";
 
+interface MembersResponse {
+  data: Member[];
+}
+
 export const useGetMember = () => {
-  return useQuery({
+  return useQuery<AxiosResponse<MembersResponse>>({
     queryKey: ["members"],
     queryFn: getMembers,
   });
