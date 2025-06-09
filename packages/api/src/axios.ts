813901f048db791fa5626228fbdc8d7f191f800b
bae080311 <s24024@gsm.hs.kr>
2025-06-09 16:07:09 +0900

    fix: lint

--- packages/api/src/axios.ts
+++ packages/api/src/axios.ts
@@ -1,10 +1,10 @@
 "use client";
 
 import { getCookie } from "@repo/utils/getCookie";
-import axios, { 
+import axios, {
   type InternalAxiosRequestConfig,
   type CreateAxiosDefaults,
-  AxiosHeaders 
+  AxiosHeaders,
 } from "axios";
 
 const TIMEOUT = 10_000;
