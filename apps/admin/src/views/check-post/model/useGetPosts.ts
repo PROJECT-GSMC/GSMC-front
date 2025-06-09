a583a713582b2f4b4809317ee92f4939e2602cc4
bae080311 <s24024@gsm.hs.kr>
2025-06-10 00:25:17 +0900

    fix: import

--- apps/admin/src/views/check-post/model/useGetPosts.ts
+++ apps/admin/src/views/check-post/model/useGetPosts.ts
@@ -1,9 +1,14 @@
-import type { postState } from "@repo/types/evidences";
+import type { EvidenceResponse, postState } from "@repo/types/evidences";
 import { useQuery } from "@tanstack/react-query";
+import type { UseQueryResult } from "@tanstack/react-query";
+import type { AxiosResponse } from "axios";
 
 import { getPosts } from "../api/getPosts";
 
-export const useGetPosts = (email: string, status: postState | null) => {
+export const useGetPosts = (
+  email: string,
+  status: postState | null
+): UseQueryResult<AxiosResponse<EvidenceResponse>> => {
   return useQuery({
     queryKey: ["posts", email, status],
     queryFn: () => getPosts(email, status),
