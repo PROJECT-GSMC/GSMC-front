0df1ed9023fd30d7fcaa14827db02d5bf3f6c62c
bae080311 <s24024@gsm.hs.kr>
2025-06-10 16:55:59 +0900

    fix: lint error

--- apps/admin/src/views/check-post/ui/posts.tsx
+++ apps/admin/src/views/check-post/ui/posts.tsx
@@ -1,40 +1,40 @@
 "use client";
 
+import { Button } from "@repo/shared/button";
+import { usePost } from "@repo/store/postProvider";
+import type { postState } from "@repo/types/evidences";
+import type { PostType } from "@repo/types/postType";
+import { useParams, useRouter } from "next/navigation";
 import { useState } from "react";
 import { toast } from "sonner";
-import { Button } from "@repo/shared/button";
-import { Post } from "../../../entities/check-post/ui/post";
-import { useGetPosts } from "../model/useGetPosts";
-import { postState } from "@repo/types/evidences";
-import { PostType } from "@repo/types/postType";
-import { useParams, useRouter } from "next/navigation";
-import { usePost } from "@repo/store/postProvider";
+
 import { useGetStudent } from "@/entities/check-post/model/useGetStudent";
+import { Post } from "@/entities/check-post/ui/post";
+
+import { useGetPosts } from "../model/useGetPosts";
 
 const PostsView = () => {
   const { setPost } = usePost();
   const [state, setState] = useState<postState>("PENDING");
-  const params = useParams();
+  const { id } = useParams();
   const R = useRouter();
-  const { data, isError, error } = useGetPosts(String(params.id), state);
+  const { data, isError } = useGetPosts(String(id), state);
   const { data: data2, isError: isError2 } = useGetStudent(
-    decodeURIComponent(String(params.id))
+    decodeURIComponent(String(id))
   );
 
-  const posts: PostType[] = [
-    ...(data?.data?.majorActivityEvidence || []),
-    ...(data?.data?.humanitiesActivityEvidence || []),
-    ...(data?.data?.readingEvidence || []),
-    ...(data?.data?.otherEvidence || []),
-  ];
+  const posts: PostType[] | [] = [
+    ...(data?.data.majorActivityEvidence ?? []),
+    ...(data?.data.humanitiesActivityEvidence ?? []),
+    ...(data?.data.readingEvidence ?? []),
+    ...(data?.data.otherEvidence ?? []),
+  ] as PostType[];
 
   if (isError) {
-    console.error(error);
     toast.error("게시글을 불러오는 데 실패했습니다.");
   }
 
   if (isError2) {
-    console.error(error);
     toast.error("사용자 정보를 불러오는 데 실패했습니다.");
   }
 
@@ -46,15 +46,18 @@ const PostsView = () => {
   return (
     <div className="flex items-center flex-col">
       <h1 className="text-tropicalblue-700 text-body1s sm:text-h4s mb-[2.06rem] mt-[2.94rem]">
-        {data2?.data?.name || "사용자"}님의 게시글
+        {data2?.data.name ?? "사용자"}님의 게시글
       </h1>
       <div className="flex gap-[5%] justify-between">
         {Buttons.map((button) => (
           <Button
             key={button.value}
             label={button.label}
-            onClick={() => setState(button.value as postState)}
             variant={state === button.value ? "blue" : "skyblue"}
+            // eslint-disable-next-line react/jsx-no-bind
+            onClick={() => {
+              setState(button.value as postState);
+            }}
           />
         ))}
       </div>
@@ -62,12 +65,13 @@ const PostsView = () => {
         {posts.length > 0 ? (
           posts.map((post: PostType) => (
             <Post
+              data={post}
+              key={post.id}
+              // eslint-disable-next-line react/jsx-no-bind
               onClick={() => {
                 R.push(`/detail/${post.id}`);
                 setPost(post);
               }}
-              key={post.id}
-              data={post}
             />
           ))
         ) : (
