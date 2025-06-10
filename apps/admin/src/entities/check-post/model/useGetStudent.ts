3b2480f1342b1f9da8ae9145cb452917afbfa253
bae080311 <s24024@gsm.hs.kr>
2025-06-10 16:56:14 +0900

    fix: return type

--- apps/admin/src/entities/check-post/model/useGetStudent.ts
+++ apps/admin/src/entities/check-post/model/useGetStudent.ts
@@ -2,9 +2,10 @@ import type { Member } from "@repo/types/member";
 import { useQuery } from "@tanstack/react-query";
 
 import { getStudent } from "../api/getStudent";
+import type { AxiosResponse } from "axios";
 
 export const useGetStudent = (email: string) => {
-  return useQuery<Member>({
+  return useQuery<AxiosResponse<Member>>({
     queryKey: ["student", email],
     queryFn: () => getStudent(email),
     staleTime: 1000 * 60 * 5,
