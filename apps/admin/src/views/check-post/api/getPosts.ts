8f3592871b113cff15d9c36f0789c69da7d5d432
bae080311 <s24024@gsm.hs.kr>
2025-06-11 10:05:28 +0900

    feat: return type fix: use slice

--- apps/admin/src/views/check-post/api/getPosts.ts
+++ apps/admin/src/views/check-post/api/getPosts.ts
@@ -1,8 +1,11 @@
 import instance from "@repo/api/axios";
-import { postState } from "@repo/types/evidences";
+import type { EvidenceResponse, postState } from "@repo/types/evidences";
 
-export const getPosts = async (email: string, status: postState | null) => {
+export const getPosts = async (
+  email: string,
+  status: postState | null
+): Promise<EvidenceResponse> => {
   return await instance.get(
-    `/evidence/${email.split("@")[0]?.substring(1)}?status=${status}`
+    `/evidence/${email.split("@")[0]?.slice(1)}?status=${status}`
   );
 };
