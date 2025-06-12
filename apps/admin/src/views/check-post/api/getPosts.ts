6497221927d3ccac3333ae6f24779bbd0bf6cdfd
bae080311 <s24024@gsm.hs.kr>
2025-06-13 08:14:23 +0900

    fix: request

--- apps/admin/src/views/check-post/api/getPosts.ts
+++ apps/admin/src/views/check-post/api/getPosts.ts
@@ -6,6 +6,6 @@ export const getPosts = async (
   status: postState | null
 ): Promise<EvidenceResponse> => {
   return await instance.get(
-    `/evidence/${email.split("@")[0]?.slice(1)}?status=${status}`
+    `/evidence/${email.split("%40")[0]?.slice(1)}?status=${status}`
   );
 };
