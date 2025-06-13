a78fb0fb8f8646bb1552456cac00fca0c1064899
Dino0204 <s24020@gsm.hs.kr>
2025-06-13 11:11:11 +0900

    fix: unused axios

--- apps/client/src/entities/signup/api/postSignup.ts
+++ apps/client/src/entities/signup/api/postSignup.ts
@@ -1,15 +1,16 @@
 import instance from "@repo/api/axios";
-import axios from "axios";
+import { isAxiosError } from "axios";
 
 import type { SignupFormProps } from "@shared/model/AuthForm";
 
 export const postSignup = async (form: SignupFormProps) => {
   try {
     const response = await instance.post(`/auth/signup`, form);
+    console.log(response.data);
     return response.data;
   } catch (error) {
-    if (axios.isAxiosError(error) && error.response) {
-      throw error.response.data || "회원가입 실패";
+    if (isAxiosError(error) && error.response) {
+      throw error.response.data ?? "회원가입 실패";
     }
     throw error;
   }
