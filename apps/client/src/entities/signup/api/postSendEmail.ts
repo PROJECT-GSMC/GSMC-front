af9665c4d814d632ff09ec3fe8d4bd1bae4fc596
Dino0204 <s24020@gsm.hs.kr>
2025-06-13 11:10:58 +0900

    unused axios

--- apps/client/src/entities/signup/api/postSendEmail.ts
+++ apps/client/src/entities/signup/api/postSendEmail.ts
@@ -1,14 +1,13 @@
 import instance from "@repo/api/axios";
-import axios from "axios";
+import { isAxiosError } from "axios";
 
 export const postSendEmail = async (email: string) => {
   try {
     const response = await instance.post(`/auth/send-email`, { email });
     return response;
   } catch (error) {
-    console.error(error);
-    if (axios.isAxiosError(error) && error.response) {
-      throw error.response.data.error || "인증 이메일 전송 실패";
+    if (isAxiosError(error) && error.response) {
+      throw error.response.data ?? "인증 이메일 전송 실패";
     }
     throw error;
   }
