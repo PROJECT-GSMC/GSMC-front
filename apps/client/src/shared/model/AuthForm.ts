171a1b550a2faf42e7896b1997c6d9bdb4354518
Dino0204 <s24020@gsm.hs.kr>
2025-06-13 10:54:17 +0900

    feat: SigninFormResponse

--- apps/client/src/shared/model/AuthForm.ts
+++ apps/client/src/shared/model/AuthForm.ts
@@ -1,12 +1,16 @@
+import type { role } from "@repo/types/member";
+
 export interface SigninFormProps {
   email: string;
   password: string;
 }
 
-export interface SignupFormProps {
-  email: string;
-  password: string;
-  name: string;
+export interface SigninFormResponse {
+  accessToken: string;
+  accessTokenExpiresAt: Date;
+  refreshToken: string;
+  refreshTokenExpiresAt: Date;
+  role: role;
 }
 
 export interface AuthStepForm {
@@ -18,4 +22,10 @@ export interface AuthStepForm {
 export interface SignupStepForm {
   password: string;
   passwordCheck: string;
+}
+
+export interface SignupFormProps {
+  email: string;
+  password: string;
+  name: string;
 }
