746d835bdfb9f72926f486e0739ab9e0cfd385c0
Dino0204 <s24020@gsm.hs.kr>
2025-06-17 08:58:16 +0900

    refactor: ChangePasswordView, useMutation

--- apps/client/src/views/changePassword/ui/index.tsx
+++ apps/client/src/views/changePassword/ui/index.tsx
@@ -1,45 +1,59 @@
 "use client";
 
+
 import { Button } from "@repo/shared/button";
 import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
+import { useCallback } from "react";
 import { useForm } from "react-hook-form";
 
 import { patchPassword } from "@/shared/api/patchPassword";
-import type { ChangePasswordProps } from "@/shared/model/changePWForm";
+import type { ServerResponse } from "@/shared/model/AuthForm";
+import type { ChangePasswordForm, ChangePasswordProps } from "@/shared/model/changePWForm";
+import type { HttpError } from "@/shared/types/error";
 import ChangePassword from "@/widgets/changePassword/ui";
 import { AuthForm } from "@widgets/auth/ui";
 
 const ChangePasswordView = () => {
   const router = useRouter()
 
-  const {
-    control,
-    handleSubmit,
-    formState: { isValid },
-  } = useForm<ChangePasswordProps>({
-    mode: "onChange",
-    defaultValues: { email: "", newPassword: "", newPasswordCheck: "" },
-  });
-
-  const { mutate: changePWMutate } = useMutation({
-    mutationFn: (form: ChangePasswordProps) => patchPassword(form),
-    onSuccess: () => { router.push("/"); },
+  const { mutate: changePWMutate } = useMutation<ServerResponse, HttpError, ChangePasswordProps>({
+    mutationFn: patchPassword,
+    onSuccess: (data) => {
+      if (data.success)
+        router.push("/");
+    },
     onError: (error: Error) => {
       throw error;
     },
   });
 
-  const onSubmit = (form: ChangePasswordProps) => {
-    changePWMutate(form);
-  };
+  const {
+    control,
+    handleSubmit,
+    formState: { isValid },
+  } = useForm<ChangePasswordForm>({
+    mode: "onChange",
+    defaultValues: { email: "", newPassword: "", newPasswordCheck: "" },
+  });
+
+  const onSubmit = useCallback((form: ChangePasswordProps) => {
+    changePWMutate({
+      email: form.email,
+      newPassword: form.newPassword
+    });
+  }, [changePWMutate]);
+
+  const handleChangePassword = useCallback((e: React.FormEvent<HTMLFormElement>) => {
+    void handleSubmit(onSubmit)(e);
+  }, [handleSubmit, onSubmit]);
 
   return (
     <div className="flex justify-center items-center h-screen bg-tropicalblue-100">
       <AuthForm label="Change Password">
         <form
           className="flex flex-col items-center w-full gap-[3.625rem]"
-          onSubmit={void handleSubmit(onSubmit)}
+          onSubmit={handleChangePassword}
         >
           <div className="flex flex-col gap-[0.75rem] self-stretch">
             <ChangePassword control={control} />
@@ -47,6 +61,7 @@ const ChangePasswordView = () => {
           <Button
             label="비밀번호 변경"
             state={isValid ? "default" : "disabled"}
+            type="submit"
             variant="blue"
           />
         </form>
