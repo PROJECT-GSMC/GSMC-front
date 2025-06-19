ff4dfd231277a52f464930e2563d25dbd1973868
bae080311 <s24024@gsm.hs.kr>
2025-06-19 13:40:20 +0900

    fix: api requests

--- apps/client/src/shared/api/sendScore.ts
+++ apps/client/src/shared/api/sendScore.ts
@@ -1,5 +1,13 @@
 import instance from "@repo/api/axios";
+import type { AxiosError, AxiosResponse } from "axios";
 
-export const sendScore = async (formData: FormData) => {
-  return await instance.post("/evidence/current/scoring", formData);
+export const sendScore = async (
+  formData: FormData
+): Promise<AxiosError | AxiosResponse> => {
+  try {
+    const res = await instance.post("/evidence/current/scoring", formData);
+    return res;
+  } catch (error) {
+    return error as AxiosError;
+  }
 };
