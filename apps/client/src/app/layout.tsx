fdca6940d088d420bd7f6ea677a97a8c69f81c2a
Dino0204 <s24020@gsm.hs.kr>
2025-06-20 11:41:37 +0900

    fix: body postion relative

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className="relative">
         <PostProvider>
           <Providers>
             <Header />
