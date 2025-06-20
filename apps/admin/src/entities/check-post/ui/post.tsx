a972680f416a621f4cabae4ea78243ec2f7598dd
Dino0204 <s24020@gsm.hs.kr>
2025-06-20 13:44:25 +0900

    remove: approve, reject in posts

--- apps/admin/src/entities/check-post/ui/post.tsx
+++ apps/admin/src/entities/check-post/ui/post.tsx
@@ -6,16 +6,12 @@ import { isActivity, isOthers, isReading } from "@repo/utils/handlePost";
 import { handleState, handleStateColor } from "@repo/utils/handleState";
 import Image from "next/image";
 
-
-import { useChangeEvidenceState } from "@/views/check-post/model/useChangeEvidenceState";
-
 interface PostProps {
   data: post;
   onClick?: () => void;
 }
 
 const Post = ({ data, onClick }: PostProps) => {
-  const { handlePostState } = useChangeEvidenceState(Number(data.id));
 
   return (
     <article
@@ -40,26 +36,9 @@ const Post = ({ data, onClick }: PostProps) => {
         <p className="text-gray-400 text-body5 mb-[0.75rem] line-clamp-2">
           {isActivity(data) || isOthers(data) ? getCategoryName(data.categoryName) : ("author" in data ? data.author : "")}
         </p>
-        {data.status === "PENDING" ? (
-          <span className="flex w-full mt-[1.25rem] gap-[1rem] items-center justify-center text-body5 text-white">
-            <button
-              className="bg-tropicalblue-500 px-[1.25rem] rounded-[0.5rem] py-[0.625rem] flex-1"
-              onClick={handlePostState("APPROVE")}
-            >
-              통과
-            </button>
-            <button
-              className="bg-errors-500 px-[1.25rem] rounded-[0.5rem] py-[0.625rem] flex-1"
-              onClick={handlePostState("REJECT")}
-            >
-              거절
-            </button>
-          </span>
-        ) : (
-          <span className={`text-body5 ${handleStateColor(data.status)}`}>
-            {handleState(data.status)}
-          </span>
-        )}
+        <span className={`text-body5 ${handleStateColor(data.status)}`}>
+          {handleState(data.status)}
+        </span>
       </section>
     </article>
   );
