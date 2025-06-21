2ed31571e321f11dcfd5de1728489cb4f5ed5795
Dino0204 <s24020@gsm.hs.kr>
2025-06-21 19:02:42 +0900

    fix: detail

--- apps/client/src/views/detail/index.tsx
+++ apps/client/src/views/detail/index.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { Button } from "@repo/shared/button";
+import type { Draft } from "@repo/types/draft";
 import type { post } from "@repo/types/evidences";
 import { getCategoryName } from "@repo/utils/handleCategory";
-import { isActivity, isOthers, isReading } from "@repo/utils/handlePost";
 import MockJson from "@shared/mocks/data/evidenceMock.json";
 import Image from "next/image";
 import { useParams, useRouter, useSearchParams } from "next/navigation";
@@ -11,47 +11,65 @@ import { useCallback, useState } from "react";
 import { toast } from "sonner";
 
 import ConfirmDetail from "@/entities/detail/ui/confirmDetail";
+import { useGetDraft } from "@/entities/posts/lib/useGetDraft";
 import { useGetPosts } from "@/entities/posts/lib/useGetPosts";
 import { useGetCurrentMember } from "@/shared/model/useGetCurrentMember";
 
 const DetailView = () => {
   const searchParams = useSearchParams();
   const example = searchParams.get("example");
+  const draft = searchParams.get("draft");
   const params = useParams();
   const router = useRouter();
   const { id } = params;
-  const { data, isError } = useGetPosts(null);
-  const { data: data2, isError: isError2 } = useGetCurrentMember();
+  const { data: postsData, isError: isPostsError } = useGetPosts(null);
+  const { data: draftsData, isError: isDraftsError } = useGetDraft();
+  const { data: studentData, isError: isStudentDataError } = useGetCurrentMember();
   const [show, setShow] = useState(false);
 
-  if (isError) {
+  if (isPostsError || isDraftsError) {
     toast.error("게시물을 불러오지 못했습니다.");
   }
 
-  if (isError2) {
+  if (isStudentDataError) {
     toast.error("회원 정보를 불러오지 못했습니다.");
   }
 
   const posts: post[] = [
-    ...(data?.data.majorActivityEvidence ?? []),
-    ...(data?.data.humanitiesActivityEvidence ?? []),
-    ...(data?.data.readingEvidence ?? []),
-    ...(data?.data.otherEvidence ?? []),
+    ...(postsData?.data.majorActivityEvidence ?? []),
+    ...(postsData?.data.humanitiesActivityEvidence ?? []),
+    ...(postsData?.data.readingEvidence ?? []),
+    ...(postsData?.data.otherEvidence ?? []),
+  ];
+
+  const draftPosts: Draft[] = [
+    ...(draftsData?.activityEvidences ?? []),
+    ...(draftsData?.readingEvidences ?? []),
   ];
 
   const Mock: post[] = MockJson as post[];
 
-  const post =
-    example == null
-      ? posts.find((post) => post.id === Number(id))
-      : Mock.find((post) => post.id === Number(id));
+  let post: post | Draft | undefined;
+
+  if (draft === "true") {
+    post = draftPosts.find((post) => post.draftId === id);
+  } else if (example === "true") {
+    post = Mock.find((post) => post.id === Number(id));
+  } else {
+    post = posts.find((post) => post.id === Number(id));
+  }
 
   const handleRevise = useCallback(() => {
     const idString = String(id);
+    if (draft === "true") {
+      router.push(`/edit/${idString}?draft=true`);
+      return;
+    }
     const exampleQuery =
       example != null && example !== "" ? "?example=true" : "";
+
     router.push(`/edit/${idString}${exampleQuery}`);
-  }, [router, id, example]);
+  }, [router, id, example, draft]);
 
   const handleBack = useCallback(() => {
     router.back();
@@ -64,17 +82,28 @@ const DetailView = () => {
   let title = "Title";
   let subTitle = "Author";
   let content = "";
+  let imageUri: string | null | undefined;
+  let fileUri: string | null | undefined;
 
   if (post) {
-    if (isActivity(post) || isReading(post)) {
+    if ("evidenceType" in post) {
+      title = post.evidenceType;
+      subTitle = `카테고리: ${getCategoryName(post.categoryName)}`;
+      content = "자세한 내용은 파일을 확인해주세요.";
+      fileUri = post.fileUri;
+    } else {
       title = post.title;
       content = post.content;
-    }
 
-    if (isActivity(post) || isOthers(post)) {
-      subTitle = `카테고리: ${getCategoryName(post.categoryName)}`;
-    } else if (isReading(post) && post.author) {
-      subTitle = post.author;
+      if ("author" in post && post.author) {
+        subTitle = post.author;
+      } else if ("categoryName" in post) {
+        subTitle = `카테고리: ${getCategoryName(post.categoryName)}`;
+      }
+
+      if ("imageUri" in post && (post.imageUri != null)) {
+        imageUri = post.imageUri;
+      }
     }
   }
 
@@ -84,52 +113,75 @@ const DetailView = () => {
         <header className="flex flex-col w-full gap-[0.5rem]">
           <h1 className="text-[2.25rem] font-semibold">{title}</h1>
           <h3 className="text-[0.75rem] text-[#767676] text-right font-normal">
-            {`${data2?.name ?? "사용자"} . ${subTitle}`}
+            {`${studentData?.name ?? "사용자"} . ${subTitle}`}
           </h3>
           <div className="w-full h-[0.5px] bg-[#A6A6A6]" />
         </header>
 
         <main className="flex flex-col gap-[3rem]">
-          {post &&
-            isActivity(post) &&
-            post.imageUri != null &&
-            post.imageUri !== "" ? (
+          {(imageUri == null) ? null : (
             <div className="h-[21.215rem] bg-slate-600">
               <Image
-                alt={post.title}
+                alt={title}
                 className="object-cover w-full h-full"
                 height={150}
-                src={post.imageUri ?? ""}
+                src={imageUri}
                 width={188}
               />
             </div>
-          ) : null}
+          )}
           <section className="flex flex-col gap-[1rem]">
             <h2 className="text-[1.5rem] font-semibold">{subTitle}</h2>
             <p className="text-[1.25rem] font-normal min-h-[29.9375rem]">
-              {content}
+              {(fileUri == null) ? (
+                content
+              ) : (
+                <a
+                  className="underline"
+                  href={fileUri}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  증빙 파일 보기
+                </a>
+              )}
             </p>
           </section>
         </main>
-        <span
-          className="text-errors-500 underline underline-offset-4 text-body5 cursor-pointer"
-          onClick={handleShow}
-        >
-          이 게시글 삭제하기
-        </span>
+        {draft == null &&
+          <span
+            className="text-errors-500 underline underline-offset-4 text-body5 cursor-pointer"
+            onClick={handleShow}
+          >
+            이 게시글 삭제하기
+          </span>
+        }
         <footer className="sticky bottom-4 flex gap-[1.56rem] w-full">
-          <Button
-            className="w-full"
-            label="수정하기"
-            variant="blue"
-            onClick={handleRevise}
-          />
-          <Button
-            className="w-full bg-white"
-            label="뒤로가기"
-            variant="skyblue"
-            onClick={handleBack}
-          />
+          {draft == null && example == null ?
+            (
+              <>
+                <Button
+                  className="w-full"
+                  label="수정하기"
+                  variant="blue"
+                  onClick={handleRevise}
+                />
+                <Button
+                  className="w-full bg-white"
+                  label="뒤로가기"
+                  variant="skyblue"
+                  onClick={handleBack}
+                />
+              </>
+            ) : (
+              example == null &&
+              <Button
+                className="w-full"
+                label="이어적기"
+                variant="blue"
+                onClick={handleRevise}
+              />
+            )}
         </footer>
       </div>
       <ConfirmDetail id={Number(id)} setShow={setShow} show={show} />
