ba9571f4d77bbec0b2df0acb5086817e96bfc9ac
Dino0204 <s24020@gsm.hs.kr>
2025-06-25 07:56:09 +0900

    feat: postSignup type

--- apps/client/src/entities/signup/api/postSignup.ts
+++ apps/client/src/entities/signup/api/postSignup.ts
@@ -1,9 +1,11 @@
 import instance from "@repo/api/axios";
-import { isAxiosError } from "axios";
+import { isAxiosError, type AxiosResponse } from "axios";
 
 import type { SignupFormProps } from "@shared/model/AuthForm";
 
-export const postSignup = async (form: SignupFormProps) => {
+export const postSignup = async (
+  form: SignupFormProps
+): Promise<AxiosResponse> => {
   try {
     const response = await instance.post(`/auth/signup`, form);
     return response;
