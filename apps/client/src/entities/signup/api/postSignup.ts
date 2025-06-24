f9c252ca63b3a12f20d7c349a25803e3c2bd880d
Dino0204 <s24020@gsm.hs.kr>
2025-06-24 23:24:25 +0900

    fix: signup response type

--- apps/client/src/entities/signup/api/postSignup.ts
+++ apps/client/src/entities/signup/api/postSignup.ts
@@ -5,11 +5,8 @@ import type { SignupFormProps } from "@shared/model/AuthForm";
 
 export const postSignup = async (form: SignupFormProps) => {
   try {
-    const response = await instance.post<{ success: boolean }>(
-      `/auth/signup`,
-      form
-    );
-    return response.data;
+    const response = await instance.post(`/auth/signup`, form);
+    return response;
   } catch (error) {
     if (isAxiosError(error) && error.response) {
       throw error.response.data ?? "회원가입 실패";
