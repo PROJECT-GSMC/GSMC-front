491c8a48b8133585f36db4f81e3c073f6e4c8abf
bae080311 <s24024@gsm.hs.kr>
2025-06-25 14:57:55 +0900

    fix: hidden overflow

--- apps/client/src/shared/ui/file/index.tsx
+++ apps/client/src/shared/ui/file/index.tsx
@@ -10,10 +10,13 @@ interface FileProps {
 }
 
 const File = ({ isImg = true, value, onChange, label }: FileProps) => {
-  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-    if (file) onChange(file);
-  }, [onChange]);
+  const handleChange = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      const file = e.target.files?.[0];
+      if (file) onChange(file);
+    },
+    [onChange]
+  );
 
   return (
     <div className="w-full flex flex-col gap-[0.5rem]">
@@ -21,11 +24,13 @@ const File = ({ isImg = true, value, onChange, label }: FileProps) => {
         {label}
       </label>
       <label
-        className="bg-white flex font-[0.875rem] text-[#B4B5B7] border gap-[0.5rem] rounded-[0.75rem] cursor-pointer w-full mt-[0.69rem] py-[0.91rem] px-[1rem]"
+        className="bg-white flex font-[0.875rem] text-[#B4B5B7] border gap-[0.5rem] rounded-[0.75rem] cursor-pointer w-full mt-[0.69rem] py-[0.91rem] px-[1rem] overflow-hidden"
         htmlFor={label}
       >
         <Chain />
-        {value ? value.name : "파일 첨부"}
+        <span className="truncate max-w-full">
+          {value ? value.name : "파일 첨부"}
+        </span>
       </label>
       <input
         accept={isImg ? "image/*" : "*/*"}
