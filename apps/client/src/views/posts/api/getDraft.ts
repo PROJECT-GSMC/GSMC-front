463e6268f0c7e6b0547c94f0a6bf4a313ebceec7
Dino0204 <s24020@gsm.hs.kr>
2025-07-02 10:28:31 +0900

    move: draft

--- apps/client/src/views/posts/api/getDraft.ts
+++
@@ -1,7 +0,0 @@
-import instance from "@repo/api/axios";
-import type { DraftResponse } from "@repo/types/draft";
-
-export const getDraft = async () => {
-  const response = await instance.get<DraftResponse>("/evidence/current/draft");
-  return response.data;
-};
