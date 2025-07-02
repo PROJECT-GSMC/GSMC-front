b6b72c2c093170bb7b86cf14aa2d00da536d8fce
Dino0204 <s24020@gsm.hs.kr>
2025-07-02 10:29:01 +0900

    move: post

---
+++ apps/client/src/shared/lib/useGetPosts.ts
@@ -0,0 +1,12 @@
+import type { EvidenceType } from "@repo/types/evidences";
+import { useQuery } from "@tanstack/react-query";
+
+import { getPosts } from "../api/getPosts";
+
+export const useGetPosts = (type: EvidenceType | "DRAFT" | null) => {
+  return useQuery({
+    queryKey: ["posts", type],
+    queryFn: () => getPosts(type as EvidenceType | null),
+    enabled: type !== "DRAFT",
+  });
+};
