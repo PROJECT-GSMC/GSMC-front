5cd582a0e1c34c3d4fee1b036996a5d37c3865f2
Dino0204 <s24020@gsm.hs.kr>
2025-07-02 10:26:57 +0900

    fix: deletePost type

--- apps/client/src/entities/detail/api/deletePost.ts
+++ apps/client/src/entities/detail/api/deletePost.ts
@@ -1,16 +1,16 @@
 import instance from "@repo/api";
-import type { AxiosError, AxiosResponse } from "axios";
+import { isAxiosError } from "axios";
 
-export const deletePost = async (
-  id: number,
-): Promise<AxiosError | AxiosResponse> => {
+export const deletePost = async (id: number) => {
   try {
     const response = await instance.delete(`/evidence/current/${id}`, {
       method: "DELETE",
     });
-
     return response;
-  } catch (error) {
-    return error as AxiosError;
+  } catch (error: unknown) {
+    if (isAxiosError(error) && error.response) {
+      throw error.response.data ?? "게시물 삭제 실패";
+    }
+    throw error;
   }
 };
