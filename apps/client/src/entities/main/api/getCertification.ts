c0acc2a2993d3f7c1ac9e3f36496e36f28a46637
Dino0204 <s24020@gsm.hs.kr>
2025-07-02 15:31:51 +0900

    fix: certification

--- apps/client/src/entities/main/api/getCertification.ts
+++ apps/client/src/entities/main/api/getCertification.ts
@@ -1,13 +1,22 @@
 import instance from "@repo/api/axios";
+import { isAxiosError } from "axios";
 
-import type { Certification } from "../model/certification";
+import type { CertificationResponse } from "@/shared/model/certification";
 
 interface Certifications {
-  data: {
-    certificates: Certification[];
-  };
+  certificates: CertificationResponse[];
 }
 
 export const getCertification = async (): Promise<Certifications> => {
-  return instance.get("/certificates/current");
+  try {
+    const response = await instance.get<Certifications>(
+      "/certificates/current"
+    );
+    return response.data;
+  } catch (error: unknown) {
+    if (isAxiosError(error) && error.response) {
+      throw error.response.data ?? "자격증 취득 실패";
+    }
+    throw error;
+  }
 };
