b61f90aa05b123f1c685541283613dd616bc4c95
Dino0204 <s24020@gsm.hs.kr>
2025-07-03 10:57:25 +0900

    fix: score

--- apps/client/src/widgets/write/api/postScore.ts
+++ apps/client/src/widgets/write/api/postScore.ts
@@ -1,13 +1,14 @@
 import instance from "@repo/api/axios";
-import type { AxiosError, AxiosResponse } from "axios";
+import { isAxiosError, type AxiosResponse } from "axios";
 
-export const postScore = async (
-  formData: FormData
-): Promise<AxiosError | AxiosResponse> => {
+export const postScore = async (formData: FormData): Promise<AxiosResponse> => {
   try {
     const res = await instance.post("/evidence/current/scoring", formData);
     return res;
-  } catch (error) {
-    return error as AxiosError;
+  } catch (error: unknown) {
+    if (isAxiosError(error) && error.response) {
+      throw error.response.data ?? "외국어 영역 저장 실패";
+    }
+    throw error;
   }
 };
