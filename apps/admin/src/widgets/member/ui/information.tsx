e2387b5168648d33ef6755306268f0dfe635c955
bae080311 <s24024@gsm.hs.kr>
2025-07-05 16:01:32 +0900

    feat: score modal

--- apps/admin/src/widgets/member/ui/information.tsx
+++ apps/admin/src/widgets/member/ui/information.tsx
@@ -2,11 +2,13 @@
 
 import { Button } from "@repo/shared/button";
 import { useRouter } from "next/navigation";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 
 import { useMember } from "@/entities/member/model/memberContext";
+import ScoreModal from "@/entities/member/ui/scoreModal";
 
 export default function Information() {
+  const [modal, setModal] = useState(false);
   const R = useRouter();
   const { member: student } = useMember();
 
@@ -17,6 +19,13 @@ export default function Information() {
   const handleScore = useCallback(() => {
     R.push(`/score/${student?.email}`);
   }, [R, student?.email]);
+
+  const open = useCallback(() => {
+    setModal(true);
+  }, []);
+  const close = useCallback(() => {
+    setModal(false);
+  }, []);
 
   if (student === undefined) {
     return <p className="text-center m-8">로딩중...</p>;
@@ -31,19 +40,23 @@ export default function Information() {
             <span className="text-body1 text-gray-600">{student.name}</span>
             <small className="text-body2 text-gray-600">{`${student.grade}학년 ${student.classNumber}반 ${student.number}번`}</small>
           </div>
-          <div className="text-tropicalblue-700 mt-[1.25rem] md:mb-[2.56rem] sm:mb-[1.75rem] mb-[3rem] text-titleMedium h-[32rem] max-md:h-[10rem] max-lg:h-[10rem] flex items-center justify-center rounded-[0.75rem] bg-white">
+          <div
+            onClick={open}
+            className="text-tropicalblue-700 mt-[1.25rem] md:mb-[2.56rem] sm:mb-[1.75rem] mb-[3rem] text-titleMedium h-[32rem] max-md:h-[10rem] max-lg:h-[10rem] flex items-center justify-center rounded-[0.75rem] bg-white"
+          >
             {student.totalScore + "점"}
           </div>
         </div>
-        <div className="flex flex-col gap-[0.75rem]">
-          <Button
-            label="글 보러가기"
-            variant="blue"
-            onClick={handleCheckPost}
-          />
-          <Button label="점수 관리" variant="skyblue" onClick={handleScore} />
-        </div>
       </div>
+      <div className="flex flex-col gap-[0.75rem]">
+        <Button label="글 보러가기" variant="blue" onClick={handleCheckPost} />
+        <Button label="점수 관리" variant="skyblue" onClick={handleScore} />
+      </div>
+      <ScoreModal
+        show={modal}
+        id={student?.email?.split("@")[0]?.slice(1) as string}
+        close={close}
+      />
     </div>
   );
 }
