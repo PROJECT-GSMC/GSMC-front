c1dd1ab6a6e44d9908a4aa27b80ff0cdd1823533
Dino0204 <s24020@gsm.hs.kr>
2025-07-05 19:21:17 +0900

    chore: getPost

--- apps/client/src/shared/api/getPosts.ts
+++ apps/client/src/shared/api/getPosts.ts
@@ -1,8 +1,6 @@
 import instance from "@repo/api/axios";
-import type { EvidenceResponse, EvidenceType } from "@repo/types/evidences";
+import type { PostResponse, EvidenceType } from "@repo/types/evidences";
 
-export const getPosts = async (type: EvidenceType | null) => {
-  return await instance.get<EvidenceResponse>(
-    `/evidence/current${type === null ? "" : "?type=" + type}`,
-  );
+export const getPosts = async (type: EvidenceType) => {
+  return await instance.get<PostResponse>(`/evidence/current?type=${type}`);
 };
--- apps/client/src/shared/lib/useGetPosts.ts
+++ apps/client/src/shared/lib/useGetPosts.ts
@@ -3,10 +3,10 @@ import { useQuery } from "@tanstack/react-query";
 
 import { getPosts } from "../api/getPosts";
 
-export const useGetPosts = (type: EvidenceType | "DRAFT" | null) => {
+export const useGetPosts = (type: EvidenceType | "DRAFT") => {
   return useQuery({
     queryKey: ["posts", type],
-    queryFn: () => getPosts(type as EvidenceType | null),
+    queryFn: () => getPosts(type as EvidenceType),
     enabled: type !== "DRAFT",
   });
 };
