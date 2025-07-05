7e3a2aa1221156fc2982fc3d84ba12dcb2b4e3cd
Dino0204 <s24020@gsm.hs.kr>
2025-07-05 19:12:18 +0900

    fix: typeGuard

--- packages/utils/src/handlePost.ts
+++ packages/utils/src/handlePost.ts
@@ -1,32 +1,31 @@
-import type { Draft, ActivityDraft, ReadingDraft } from "@repo/types/draft";
-import type { Activity, Reading, Others } from "@repo/types/evidences";
+import type { DraftType, ActivityDraft, ReadingDraft } from "@repo/types/draft";
+import type {
+  Activity,
+  Reading,
+  Others,
+  PostType,
+} from "@repo/types/evidences";
 
-interface MockPostFlag {
-  __isMock: true;
+function isDraft(data: PostType | DraftType): data is DraftType {
+  return typeof data === "object" && "draftId" in data;
 }
 
-export type MockPost = (Activity | Reading | Others) & MockPostFlag;
+function isPost(data: PostType | DraftType): data is PostType {
+  return typeof data === "object" && "id" in data;
+}
 
-const isActivity = (
-  data: Activity | Reading | Others | Draft
-): data is Activity | ActivityDraft => {
+function isActivity(
+  data: PostType | DraftType
+): data is Activity | ActivityDraft {
   return "imageUri" in data;
-};
+}
 
-const isReading = (
-  data: Activity | Reading | Others | Draft
-): data is Reading | ReadingDraft => {
+function isReading(data: PostType | DraftType): data is Reading | ReadingDraft {
   return "author" in data;
-};
+}
 
-const isOthers = (
-  data: Activity | Reading | Others | Draft
-): data is Others => {
+function isOthers(data: PostType | DraftType): data is Others {
   return !isActivity(data) && !isReading(data);
-};
+}
 
-const isMockPost = (data: unknown): data is MockPost => {
-  return typeof data === "object" && data !== null && "__isMock" in data;
-};
-
-export { isActivity, isReading, isOthers, isMockPost };
+export { isDraft, isPost, isActivity, isReading, isOthers };
