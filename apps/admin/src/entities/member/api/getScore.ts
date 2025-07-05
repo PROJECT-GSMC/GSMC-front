4f42a74fbb795396103d589b6b888f22b9b817ca
bae080311 <s24024@gsm.hs.kr>
2025-07-05 15:57:03 +0900

    feat: get score

---
+++ apps/admin/src/entities/member/api/getScore.ts
@@ -0,0 +1,18 @@
+import instance from "@repo/api";
+import { AxiosError } from "axios";
+import { toast } from "sonner";
+import type { ScoreResponse } from "../model/score";
+
+export const getScore = async (
+  id: string
+): Promise<ScoreResponse | undefined> => {
+  try {
+    const res = await instance.get(`/score/${id}`);
+    return res.data as ScoreResponse;
+  } catch (error) {
+    if (error instanceof AxiosError) {
+      toast.error("점수 정보를 불러오는데 실패했습니다");
+    }
+  }
+  return undefined;
+};
---
+++ apps/admin/src/entities/member/model/useGetScore.ts
@@ -0,0 +1,11 @@
+import { useQuery } from "@tanstack/react-query";
+import { getScore } from "../api/getScore";
+import type { ScoreResponse } from "./score";
+
+export const useGetScore = (id: string) => {
+  return useQuery<ScoreResponse | undefined>({
+    queryKey: ["score", id],
+    queryFn: () => getScore(id),
+    enabled: !!id,
+  });
+};
