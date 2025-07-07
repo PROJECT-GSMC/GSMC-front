32952ccb57a2af76b01d5e67ae59552bd7eba2d9
Dino0204 <s24020@gsm.hs.kr>
2025-07-07 10:58:49 +0900

    fix: example postType

--- apps/client/src/widgets/example/ui/index.tsx
+++ apps/client/src/widgets/example/ui/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { usePost } from "@repo/store/postProvider";
-import type { post } from "@repo/types/evidences";
+import type { PostType } from "@repo/types/evidences";
 import { useRouter } from "next/navigation";
 import { useCallback } from "react";
 
@@ -12,17 +12,17 @@ export default function ExampleWidget() {
   const { setPost } = usePost();
   const router = useRouter();
 
-  const handleRoute = useCallback((mock: post) => () => {
+  const handleRoute = useCallback((mock: PostType) => () => {
     setPost(mock);
-    router.push(`/detail/${mock.id}`);
+    router.push(`/detail/${mock.id}?example=${true}`);
   }, [router, setPost]);
 
-  const mockPosts: post[] = [
-    ...(Mock.majorActivityEvidence as post[]),
-    ...(Mock.humanitiesActivityEvidence as post[]),
-    ...(Mock.readingEvidence as post[]),
-    ...(Mock.otherEvidence as post[]),
-  ];
+  const mockPosts: PostType[] = [
+    ...(Mock.majorActivityEvidence),
+    ...(Mock.humanitiesActivityEvidence),
+    ...(Mock.readingEvidence),
+    ...(Mock.otherEvidence),
+  ] as PostType[];
 
   return (
     <div className="flex mt-[2.69rem] overflow-y-visible flex-wrap sm:justify-start justify-center w-full gap-[1.12rem]">
