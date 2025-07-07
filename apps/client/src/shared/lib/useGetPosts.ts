e14cb0ace77b720c5a105fc4622fdba08e92eb03
Dino0204 <s24020@gsm.hs.kr>
2025-07-07 17:52:59 +0900

    feat: posts

--- apps/client/src/shared/lib/useGetPosts.ts
+++ apps/client/src/shared/lib/useGetPosts.ts
@@ -1,12 +1,24 @@
-import type { EvidenceType } from "@repo/types/evidences";
+import type { EvidenceType, PostType } from "@repo/types/evidences";
 import { useQuery } from "@tanstack/react-query";
 
 import { getPosts } from "../api/getPosts";
 
 export const useGetPosts = (type: EvidenceType | "DRAFT") => {
-  return useQuery({
+  const query = useQuery({
     queryKey: ["posts", type],
     queryFn: () => getPosts(type as EvidenceType),
     enabled: type !== "DRAFT",
   });
+
+  const posts: PostType[] = [
+    ...(query.data?.data.majorActivityEvidence ?? []),
+    ...(query.data?.data.humanitiesActivityEvidence ?? []),
+    ...(query.data?.data.readingEvidence ?? []),
+    ...(query.data?.data.otherEvidence ?? []),
+  ];
+
+  return {
+    ...query,
+    posts,
+  };
 };
