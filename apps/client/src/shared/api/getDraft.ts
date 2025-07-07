850c0612e3e138a5b4cf952dc6aab0f617181c83
Dino0204 <s24020@gsm.hs.kr>
2025-07-07 17:53:20 +0900

    feat: drafts

--- apps/client/src/shared/api/getDraft.ts
+++ apps/client/src/shared/api/getDraft.ts
@@ -2,12 +2,12 @@ import instance from "@repo/api/axios";
 import type { DraftResponse } from "@repo/types/draft";
 import { isAxiosError } from "axios";
 
-export const getDraft = async (): Promise<DraftResponse> => {
+export const getDraft = async () => {
   try {
     const response = await instance.get<DraftResponse>(
       "/evidence/current/draft"
     );
-    return response.data;
+    return response;
   } catch (error) {
     if (isAxiosError(error) && error.response) {
       throw error.response.data ?? "임시저장 게시물 조회 실패";
--- apps/client/src/shared/lib/useGetDraft.ts
+++ apps/client/src/shared/lib/useGetDraft.ts
@@ -1,12 +1,23 @@
+import type { DraftType } from "@repo/types/draft";
 import type { EvidenceType } from "@repo/types/evidences";
 import { useQuery } from "@tanstack/react-query";
 
 import { getDraft } from "../api/getDraft";
 
 export const useGetDraft = (type: EvidenceType | "DRAFT" | null) => {
-  return useQuery({
+  const query = useQuery({
     queryKey: ["drafts", type],
     queryFn: getDraft,
     enabled: type == "DRAFT",
   });
+
+  const drafts: DraftType[] = [
+    ...(query.data?.data.activityEvidences ?? []),
+    ...(query.data?.data.readingEvidences ?? []),
+  ];
+
+  return {
+    ...query,
+    drafts,
+  };
 };
