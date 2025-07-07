3b0f018b9b20f194b6e930ab88a70cfab201aa1f
Dino0204 <s24020@gsm.hs.kr>
2025-07-07 17:23:16 +0900

    fix: featScore type

--- apps/admin/src/views/score/api/featScore.ts
+++ apps/admin/src/views/score/api/featScore.ts
@@ -1,12 +1,7 @@
 import instance from "@repo/api/axios";
 import { isAxiosError, type AxiosResponse } from "axios";
 
-interface FeatScoreResponse {
-  categoryName: string;
-  value: number;
-}
-
-interface FeatScoreRequest {
+interface FeatScore {
   categoryName: string;
   value: number;
 }
@@ -15,14 +10,14 @@ export const featScore = async (
   email: string,
   category: string,
   score: number
-): Promise<AxiosResponse<FeatScoreResponse>> => {
-  const data: FeatScoreRequest = {
+): Promise<AxiosResponse> => {
+  const data: FeatScore = {
     categoryName: category,
     value: Number(score),
   };
   try {
     const id = email.split("@")[0]?.slice(1);
-    return await instance.patch<FeatScoreResponse>(`/score/${id}`, data);
+    return await instance.patch<FeatScore>(`/score/${id}`, data);
   } catch (error) {
     if (isAxiosError(error) && error.response) {
       throw error;
