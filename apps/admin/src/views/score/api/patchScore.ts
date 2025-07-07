6581ff2bbe02e8aa0d19cd3955a06b836c80f2d1
Dino0204 <s24020@gsm.hs.kr>
2025-07-07 17:30:39 +0900

    rename: featScore

--- apps/admin/src/views/score/api/patchScore.ts
+++ apps/admin/src/views/score/api/patchScore.ts
@@ -1,23 +1,23 @@
 import instance from "@repo/api/axios";
 import { isAxiosError, type AxiosResponse } from "axios";
 
-interface FeatScore {
+interface PatchScore {
   categoryName: string;
   value: number;
 }
 
-export const featScore = async (
+export const patchScore = async (
   email: string,
   category: string,
   score: number
 ): Promise<AxiosResponse> => {
-  const data: FeatScore = {
+  const data: PatchScore = {
     categoryName: category,
     value: Number(score),
   };
   try {
     const id = email.split("@")[0]?.slice(1);
-    return await instance.patch<FeatScore>(`/score/${id}`, data);
+    return await instance.patch<PatchScore>(`/score/${id}`, data);
   } catch (error) {
     if (isAxiosError(error) && error.response) {
       throw error;
