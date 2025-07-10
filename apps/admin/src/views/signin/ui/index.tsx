18ef99084a0b3bb7e24d49adbcb2729dd770acaa
Dino0204 <s24020@gsm.hs.kr>
2025-07-10 12:07:25 +0900

    refactor: signin

--- apps/admin/src/views/signin/ui/index.tsx
+++ apps/admin/src/views/signin/ui/index.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 import { postSignin } from "@/entities/signin/api/postSignin";
+import type { HttpError } from "@/shared/model/error";
+import type { SigninFormProps } from "@/shared/model/signin";
 import { Button } from "@repo/shared/button";
+import { EyeClose } from "@repo/shared/eyeClose";
+import { EyeOpen } from "@repo/shared/eyeOpen";
 import { Input } from "@repo/shared/input";
 import { InputContainer } from "@repo/shared/inputContainer";
 import { setCookie } from "@repo/utils/setCookie";
@@ -10,21 +14,19 @@ import { AuthForm } from "@widgets/auth/ui";
 import { HttpStatusCode } from "axios";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
-
-import type { SigninFormProps } from "@/shared/model/AuthForm";
-import type { HttpError } from "@/shared/types/error";
 
 const SigninView = () => {
   const queryClient = useQueryClient();
   const router = useRouter();
+  const [showPassword, setShowPassword] = useState<boolean>(false)
 
   const {
     control,
     handleSubmit,
-    formState: { isValid },
+    formState: { isValid, errors },
   } = useForm<SigninFormProps>({
     mode: "onChange",
     defaultValues: { email: "", password: "" },
@@ -32,14 +34,11 @@ const SigninView = () => {
 
   const { mutate: signinMutate } = useMutation({
     mutationFn: (form: SigninFormProps) => postSignin(form),
-    onSuccess: async (data: {
-      accessToken?: string;
-      refreshToken?: string;
-    }) => {
-      if (data.accessToken !== undefined) {
+    onSuccess: async (data) => {
+      if (data.accessToken) {
         setCookie("accessToken", data.accessToken, 1);
       }
-      if (data.refreshToken !== undefined) {
+      if (data.refreshToken) {
         setCookie("refreshToken", data.refreshToken);
       }
 
@@ -47,13 +46,13 @@ const SigninView = () => {
         queryKey: ["auth"],
         exact: false,
       });
+
       router.push("/");
-      return data;
     },
     onError: (error: HttpError) => {
-      if (error.httpStatus === HttpStatusCode.Unauthorized) {
+      if (error.httpStatus == HttpStatusCode.Unauthorized) {
         toast.error("비밀번호가 올바르지 않습니다.");
-      } else if (error.httpStatus === HttpStatusCode.NotFound) {
+      } else if (error.httpStatus == HttpStatusCode.NotFound) {
         toast.error("회원가입되지 않은 계정입니다.");
         router.push("signup");
       }
@@ -74,6 +73,10 @@ const SigninView = () => {
     [handleSubmit, onSubmit],
   );
 
+  const handleShowPassword = useCallback(() => {
+    setShowPassword(!showPassword)
+  }, [showPassword])
+
   return (
     <div className="flex justify-center items-center h-screen bg-tropicalblue-100">
       <AuthForm label="LOG IN">
@@ -83,28 +86,46 @@ const SigninView = () => {
             onSubmit={handleFormSubmit}
           >
             <div className="flex flex-col gap-[0.75rem] self-stretch">
-              <InputContainer label="이메일">
+              <InputContainer error={errors.email} htmlFor="email" label="이메일">
                 <Input
                   control={control}
                   name="email"
                   rules={{
                     required: "이메일을 필수로 입력해야 합니다.",
+                    pattern: {
+                      value: /^s\d{5}@gsm\.hs\.kr$/,
+                      message: "@gsm.hs.kr 학교 이메일을 입력해주세요",
+                    },
                   }}
                 />
               </InputContainer>
-              <InputContainer label="비밀번호">
-                <Input
-                  control={control}
-                  name="password"
-                  rules={{
-                    minLength: {
-                      message: "영문, 숫자를 포함한 8자 이상으로 입력해주세요.",
-                      value: 8,
-                    },
-                    required: "비밀번호을 필수로 입력해야 합니다.",
-                  }}
-                  type="password"
-                />
+              <InputContainer error={errors.password} htmlFor="password" label="비밀번호">
+                <div className="relative w-full">
+                  <Input
+                    className="pr-10"
+                    control={control}
+                    name="password"
+                    rules={{
+                      required: "비밀번호을 필수로 입력해야 합니다.",
+                      minLength: {
+                        value: 8,
+                        message: "영문, 숫자를 포함한 8자 이상으로 입력해주세요.",
+                      },
+                      pattern: {
+                        value: /^(?=.*[a-zA-Z])(?=.*\d).*$/,
+                        message: "영문, 숫자를 포함한 비밀번호를 입력해주세요.",
+                      },
+                    }}
+                    type={showPassword ? "text" : "password"}
+                  />
+                  <button
+                    className="absolute w-6 h-6 top-[0.75rem] right-3 text-gray-500 hover:text-gray-700"
+                    type="button"
+                    onClick={handleShowPassword}
+                  >
+                    {showPassword ? <EyeOpen /> : <EyeClose />}
+                  </button>
+                </div>
               </InputContainer>
             </div>
             <Button
