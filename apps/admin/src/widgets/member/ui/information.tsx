a3ed523ebef984be28cc39c37b747746ff08cdb1
Dino0204 <s24020@gsm.hs.kr>
2025-07-14 02:03:05 +0900

    feat: countUp

--- apps/admin/src/widgets/member/ui/information.tsx
+++ apps/admin/src/widgets/member/ui/information.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Button } from "@repo/shared/button";
+import CountUp from "@repo/shared/countUp"
 import { useRouter } from "next/navigation";
 import { useCallback, useState } from "react";
 
@@ -33,26 +34,34 @@ export default function Information() {
   }
 
   return (
-    <div className="flex flex-col h-full bg-tropicalblue-100 rounded-[1.25rem] px-[2.45rem] py-[2.25rem] justify-between ">
-      <h3 className="text-titleSmall text-tropicalblue-700 my-3">인적사항</h3>
-      <div className="overflow-y-auto">
-        <div>
-          <div className="flex flex-col gap-[0.5rem]">
-            <span className="text-body1 text-gray-600">{student.name}</span>
-            <small className="text-body2 text-gray-600">{`${student.grade}학년 ${student.classNumber}반 ${student.number}번`}</small>
-          </div>
-          <div
-            className="text-tropicalblue-700 cursor-pointer mt-[1.25rem] md:mb-[2.56rem] sm:mb-[1.75rem] mb-[3rem] text-titleMedium h-[32rem] max-md:h-[10rem] max-lg:h-[10rem] flex items-center justify-center rounded-[0.75rem] bg-white"
-            onClick={open}
-          >
-            {student.totalScore + "점"}
-          </div>
-        </div>
-      </div>
-      <div className="flex flex-col gap-[0.75rem]">
+    <div className="flex flex-col justify-between h-full bg-tropicalblue-100 rounded-[1.25rem] px-[2.45rem] py-[2.25rem]">
+      <h3 className="text-titleSmall text-tropicalblue-700 my-3">
+        인적사항
+      </h3>
+      <main className="overflow-y-auto">
+        <section className="flex flex-col gap-[0.5rem]">
+          <span className="text-body1 text-gray-600">
+            {student.name}
+          </span>
+          <small className="text-body2 text-gray-600">
+            {`${student.grade}학년 ${student.classNumber}반 ${student.number}번`}
+          </small>
+        </section>
+        <h4 onClick={open}>
+          <CountUp
+            className="text-tropicalblue-700 bg-white cursor-pointer 
+              mt-[1.25rem]  mb-[3rem] text-titleMedium h-[32rem] 
+              md:mb-[2.56rem] sm:mb-[1.75rem] max-md:h-[10rem] max-lg:h-[10rem] 
+              flex items-center justify-center rounded-[0.75rem]"
+            to={student.totalScore}
+          />
+        </h4>
+      </main>
+      <footer className="flex flex-col gap-[0.75rem]">
         <Button label="글 보러가기" variant="blue" onClick={handleCheckPost} />
         <Button label="점수 관리" variant="skyblue" onClick={handleScore} />
-      </div>
+      </footer>
+
       <ScoreModal
         close={close}
         id={extractStudentCode(student.email) ?? ""}
