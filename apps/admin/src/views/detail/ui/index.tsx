52c77489a96d2bb8a3ed3ff1074c39dab5da911a
Dino0204 <s24020@gsm.hs.kr>
2025-07-13 13:16:46 +0900

    fix: detail ui

--- apps/admin/src/views/detail/ui/index.tsx
+++ apps/admin/src/views/detail/ui/index.tsx
@@ -10,13 +10,14 @@ import { toast } from "sonner";
 
 import { useMember } from "@/entities/member/model/memberContext";
 import { useGetMember } from "@/shared/model/useGetMember";
-import { useChangeEvidenceState } from "@/views/check-post/model/useChangeEvidenceState";
+import { useChangeEvidenceStatus } from "@/views/check-post/model/useChangeEvidenceStatus";
 import { useGetPosts } from "@/views/check-post/model/useGetPosts";
 
 export default function DetailView() {
   const { id } = useParams();
   const { member: student, setMember } = useMember();
-  const { handlePostState } = useChangeEvidenceState(Number(id));
+  const { handlePostState } = useChangeEvidenceStatus(Number(id));
+
   const searchParams = useSearchParams();
   const email = searchParams.get("email");
   const status = searchParams.get("status") as PostStatus;
@@ -24,6 +25,7 @@ export default function DetailView() {
   const { data: studentData, isError: isStudentError } = useGetMember(
     decodeURIComponent(String(student?.email ?? email)),
   );
+
   const { posts, isError: isPostError } = useGetPosts(
     String(student?.email ?? email),
     status,
@@ -45,64 +47,64 @@ export default function DetailView() {
 
   const post = posts.find((post) => post.id === Number(id));
 
-  let title = "Title";
-  let subTitle = "Author";
-  let content = "";
+  if (!post) return <p>존재하지 않는 게시물입니다.</p>;
 
-  if (post) {
-    if (isActivity(post) || isReading(post)) {
-      title = post.title;
-      content = post.content;
-    }
+  const title = (() => {
+    if (isActivity(post) || isReading(post)) return post.title;
+    if (isOthers(post)) return post.evidenceType
+    return null;
+  })();
 
-    if (isActivity(post) || isOthers(post)) {
-      subTitle = `카테고리: ${getCategoryName(post.categoryName)}`;
-    } else if (isReading(post) && post.author) {
-      subTitle = post.author;
-    }
-  }
+  const imageUri = isActivity(post) ? post.imageUri : null;
+
+  const subTitle = (() => {
+    if (isReading(post)) return `${post.author}`;
+    if (isOthers(post)) return getCategoryName(post.categoryName);
+    if (isActivity(post)) return getCategoryName(post.categoryName);
+    return null;
+  })();
 
   return (
     <div className="flex flex-col items-center mt-12 px-4 sm:px-8">
-      <div className="flex flex-col w-full max-w-[37.5rem] gap-[1.75rem]">
-        <header className="flex flex-col w-full max-w-[600px] gap-2">
-          <h1 className="text-[1.7rem] font-semibold sm:text-[2.25rem] ">
+      <div className="flex flex-col w-full max-w-[600px] gap-7">
+        <header className="flex flex-col w-full gap-2">
+          <h1 className="text-[1.7rem] font-semibold sm:text-[2.25rem]">
             {title}
           </h1>
-          <h3 className="text-sm text-[#767676] text-right font-normal">
-            {post && isReading(post) ? post.author : "사용자"} {" . "}
-            {post && (isActivity(post) || isOthers(post))
-              ? getCategoryName(post.categoryName)
-              : "Area"}
-          </h3>
           <div className="w-full h-px bg-[#A6A6A6]" />
         </header>
-
         <main className="flex flex-col gap-12">
-          {post &&
-            isActivity(post) &&
-            post.imageUri != null &&
-            post.imageUri !== "" ? (
-            <div className="max-h-[21.215rem]  w-full aspect-video bg-slate-600">
+          {imageUri == null ? null : (
+            <div className="max-h-[21.215rem] w-full aspect-video bg-slate-600">
               <Image
-                alt={post.title}
+                alt="gsmc"
                 className="object-cover w-full h-full"
-                height={150}
-                src={post.imageUri ?? ""}
-                width={188}
+                height={337}
+                src={imageUri}
+                width={600}
               />
             </div>
-          ) : null}
+          )}
           <section className="flex flex-col gap-4">
             <h2 className="text-xl font-semibold">{subTitle}</h2>
-            <p className="text-[1.25rem] font-normal min-h-[29.9375rem]">
-              {content}
+            <p className="text-lg font-normal min-h-[400px] break-words">
+              {isOthers(post) ? (
+                <a
+                  className="underline"
+                  href={post.fileUri}
+                  rel="noopener noreferrer"
+                  target="_blank"
+                >
+                  증빙 파일 보기
+                </a>
+              ) : (
+                post.content
+              )}
             </p>
           </section>
         </main>
-
         <footer className="sticky bottom-4 flex gap-[1.56rem] w-full">
-          {post?.status === "PENDING" ? (
+          {post.status === "PENDING" ? (
             <span className="flex w-full mt-[1.25rem] gap-[1rem] items-center justify-center text-body5 text-white">
               <button
                 className="bg-tropicalblue-500 px-[1.25rem] rounded-[0.5rem] py-[0.625rem] flex-1"
