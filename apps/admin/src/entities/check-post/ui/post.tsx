2dd19cfd9f67570857d169f7d170f0fa3e95ae8d
Dino0204 <s24020@gsm.hs.kr>
2025-07-13 13:16:36 +0900

    fix: post ui

--- apps/admin/src/entities/check-post/ui/post.tsx
+++ apps/admin/src/entities/check-post/ui/post.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import type { PostType } from "@repo/types/evidences";
 import { getCategoryName } from "@repo/utils/handleCategory";
 import { isActivity, isOthers, isReading } from "@repo/utils/handlePost";
@@ -12,36 +10,50 @@ interface PostProps {
 }
 
 const Post = ({ data, onClick }: PostProps) => {
+
+  const title = (() => {
+    if ("title" in data) return data.title;
+    if ("evidenceType" in data) return data.evidenceType
+    return null;
+  })();
+
+  const imageUri = isActivity(data) ? data.imageUri : null;
+
+  const state = "status" in data ? data.status : null;
+
+  const subTitle = (() => {
+    if (isReading(data)) return `${data.author}`;
+    if (isOthers(data)) return getCategoryName(data.categoryName);
+    if (isActivity(data)) return getCategoryName(data.categoryName);
+    return null;
+  })();
+
   return (
     <article
       className="flex flex-col w-[188px] cursor-pointer rounded-[0.625rem] h-[276px]"
       onClick={onClick}
     >
       <div className="bg-gray-400 w-full h-[150px] rounded-t-[0.625rem] overflow-hidden">
-        {isActivity(data) && data.imageUri != null ? (
+        {imageUri == null ? null : (
           <Image
-            alt={data.title}
-            className="object-cover w-[188px] h-[150px] rounded-t-[0.625rem]"
+            alt="gsmc"
+            className="object-cover rounded-t-[0.625rem]"
             height={150}
-            src={data.imageUri}
+            src={imageUri}
             width={188}
           />
-        ) : null}
+        )}
       </div>
+
       <section className="px-[0.38rem] py-[0.75rem]">
-        <h3 className="text-body2 mb-[1rem] line-clamp-2">
-          {isActivity(data) || isReading(data) ? data.title : null}
-        </h3>
+        <h3 className="text-body2 mb-[1rem] line-clamp-2">{title}</h3>
         <p className="text-gray-400 text-body5 mb-[0.75rem] line-clamp-2">
-          {isActivity(data) || isOthers(data)
-            ? getCategoryName(data.categoryName)
-            : ("author" in data
-              ? data.author
-              : "")}
+          {subTitle}
         </p>
-        <span className={`text-body5 ${handleStateColor(data.status)}`}>
-          {handleState(data.status)}
-        </span>
+        {state ?
+          <span className={`text-body5 ${handleStateColor(state)} `}>
+            {handleState(state)}
+          </span> : null}
       </section>
     </article>
   );
